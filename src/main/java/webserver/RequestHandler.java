3995ecdb694896822fb9cfcb9f273a4e0e2e84c5
k000927 <kimsungjin927@gmail.com>
2024-07-02 15:01:07 +0900

    refactor: getContentType 별도 작성

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -27,28 +27,24 @@ public class RequestHandler implements Runnable {
 
             // 로그 출력 후 요청 주소 반환
             String url = RequestLogging.printRequest(br);
+            String path = "./src/main/resources/static";
 
             if (url.equals("/")) {
                 byte[] body = "<h1>Hello World</h1>".getBytes();
                 response200Header(dos, body.length, "text/html");
                 responseBody(dos, body);
                 return;
+            } else if (url.equals("/registration")) {
+                url += "/index.html";
             }
+            path += url;
 
-            String path = "./src/main/resources/static" + url;
             byte[] body = FileHandler.getFileContent(path);
 
             String[] tokens = url.split("\\.");
             String type = tokens[tokens.length - 1];
 
-            if(type.equals("html")) response200Header(dos, body.length, "text/html");
-            else if(type.equals("css")) response200Header(dos, body.length, "text/css");
-            else if(type.equals("js")) response200Header(dos, body.length, "text/javascript");
-            else if(type.equals("ico")) response200Header(dos, body.length, "image/vnd.microsoft.icon");
-            else if(type.equals("png")) response200Header(dos, body.length, "image/png");
-            else if(type.equals("jpg")) response200Header(dos, body.length, "image/jpg");
-            else if(type.equals("svg")) response200Header(dos, body.length, "image/svg+xml");
-
+            response200Header(dos, body.length, getContentType(type));
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
@@ -73,5 +69,19 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private String getContentType(String type){
+        String contentType = "*/*";
+
+        if (type.equals("html")) contentType =  "text./css";
+        else if (type.equals("css")) contentType =  "text/css";
+        else if (type.equals("js")) contentType =  "text/javascript";
+        else if (type.equals("ico")) contentType =  "image/vnd.microsoft.icon";
+        else if (type.equals("png")) contentType =  "image/png";
+        else if (type.equals("jpg")) contentType =  "image/jpg";
+        else if (type.equals("svg")) contentType =  "image/svg+xml";
+
+        return contentType;
     }
 }
