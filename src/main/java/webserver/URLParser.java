3196afbfe38a923eb6cc167764cf72b3717da606
DrRivaski <leesm990708@gmail.com>
2024-07-02 15:29:18 +0900

    [mod] URLParser 메모리 최적화 - 받아온 사용자의 request를 새로운 String배열을 선언하지 않고 바로 return하도록 수정

--- src/main/java/webserver/URLParser.java
+++ src/main/java/webserver/URLParser.java
@@ -1,25 +1,11 @@
 package webserver;
 
 public class URLParser {
-    public boolean parseURL(String line) {
-        String[] splitedURL = line.split(" ");
-        if (splitedURL[0].equals("GET")) {
-            System.out.println(splitedURL[0]);
-        }
-        return false;
-    }
-
     public String getGetURL(String line) {
-        String[] splitedURL = line.split(" ");
-        String resource = splitedURL[1];
-        if (resource.equals("/index.html")) {
-            return resource;
-        }
-        return splitedURL[1];
+        return line.split(" ")[1];
     }
 
     public String getHttpMethod(String line) {
-        String[] splitedURL = line.split(" ");
-        return splitedURL[0];
+        return line.split(" ")[0];
     }
 }
