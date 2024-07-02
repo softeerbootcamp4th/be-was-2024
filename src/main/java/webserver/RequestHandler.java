e121bf758d645d7f1f89b0730f17830ba7dbc099
wjddn2165 <qowjddn2165@gmail.com>
2024-07-02 11:30:25 +0900

    http 요청에서 url을 파싱하여 해당 리소스를 반환하도록 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,10 +22,40 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            StringBuilder sb = new StringBuilder();
+
+            // 모든 요청 내용 읽기
+            String startLine = br.readLine();
+
+            String readLine = null;
+            while (!(readLine = br.readLine()).isEmpty()) {
+                sb.append(readLine);
+            }
+
+            // 읽은 내용 로깅
+            String request = sb.toString();
+            logger.debug(request);
+
+
+            // 요청 URL 파싱
+            String[] tokens = startLine.split(" ");
+            String url = tokens[1];
+
+            logger.info("요청 URL: " + url);
+
+            // index.html 읽기
+            String filePath = "/Users/jungwoo/Desktop/study/be-was-2024/src/main/resources/static" + url;
+            logger.info("파일 경로: " + filePath);
+            byte[] body = Files.readAllBytes(new File(filePath).toPath());
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "<h1>Hello World</h1>".getBytes();
+
+//            byte[] body = "<h1>Hello World</h1>".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
+
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
