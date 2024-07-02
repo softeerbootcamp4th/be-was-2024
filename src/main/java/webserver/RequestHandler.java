991478c72a453abf1b18b5942ca1a4242b0247af
luna156 <v010309@naver.com>
2024-07-02 16:04:02 +0900

    week1-task3 commit 3 (회원가입 기능 추가)
    RequestHandler.java
    일반적인 request 구문과 ?가 들어간 쿼리 구문들을 구분해서 처리하는 로직 추가
    회원가입 요청시 db에 user를 추가하는 registraion 추가
    회원가입 완료시 다시 메인화면으로 redirection 시키는 헤더인redirection 추가
    404 에러시 전송할 헤더 함수 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,12 +5,14 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String root = "./src/main/resources/static";
 
     private Socket connection;
 
@@ -33,7 +35,7 @@ public class RequestHandler implements Runnable {
 
             logger.info("////// request header start //////");
             String line = br.readLine();
-            String[] tokens = line.split(" ");
+            String[] startline = line.split(" ");
             logger.info(line);
             while(!line.isEmpty()){
                 line = br.readLine();
@@ -43,48 +45,94 @@ public class RequestHandler implements Runnable {
             logger.info(header.toString());
             logger.info("////// request header end //////");
 
-
-            String pathname = "./src/main/resources/static" + tokens[1];
-            logger.info("pathname : {}", pathname);
-            byte[] body = Files.readAllBytes(new File(pathname).toPath());
-
-            switch (tokens[1].split("\\.")[1]) { //content type에 따른 response
-                case "html":
-                    response200Header(dos, body.length,"text/html;charset=utf-8");
-                    responseBody(dos, body);
-                    break;
-                case "css":
-                    response200Header(dos, body.length,"text/css;charset=utf-8");
-                    responseBody(dos, body);
-                    break;
-                case "js":
-                    response200Header(dos, body.length,"text/javascript;charset=utf-8");
-                    responseBody(dos, body);
-                    break;
-                case "ico":
-                    response200Header(dos, body.length,"image/x-icon");
-                    responseBody(dos, body);
-                    break;
-                case "png":
-                    response200Header(dos, body.length,"image/png");
-                    responseBody(dos, body);
-                    break;
-                case "jpg":
-                    response200Header(dos, body.length,"image/jpeg");
-                    responseBody(dos, body);
-                    break;
-                case "svg":
-                    response200Header(dos, body.length,"image/svg+xml");
-                    responseBody(dos,body);
-                    break;
-                default:
-                    response405Header(dos);
-                    break;
-            }
-
+            responseByPath(dos, startline, header);
 
         } catch (IOException e) {
             logger.error(e.getMessage());
+        }
+    }
+
+    private void responseByPath(DataOutputStream dos, String[] startline, Map header) throws IOException {
+
+        if(startline[1].contains("?")){
+            String[] path = startline[1].split("\\?");
+            switch (path[0]){
+                case "/registration":
+                    registration(path[1]);
+                    redirection(dos, "http://localhost:8080/");
+                default:
+                    response404Header(dos);
+            }
+        }else{
+            if(!startline[1].contains(".")) startline[1] = startline[1] + "/index.html";
+            String pathname = root + startline[1];
+            byte[] body = Files.readAllBytes(new File(pathname).toPath());
+            responseByContentType(dos, body, startline[1].split("\\.")[1]);
+        }
+    }
+
+    private void registration (String params){ //사용자 등록
+        String id = "";
+        String username = "";
+        String password = "";
+        for(String line : params.split("&")){
+            String[] tmp = line.split("=");
+            if(Objects.equals(tmp[0], "id")){
+                id = tmp[1];
+            }else if(Objects.equals(tmp[0], "username")){
+                username = tmp[1];
+            }else if(Objects.equals(tmp[0], "password")){
+                password = tmp[1];
+            }
+        }
+        WebServer.addUser(id, username, password,"");
+    }
+
+    private void redirection(DataOutputStream dos, String redirectpath){ //redirection
+        try {
+            dos.writeBytes("HTTP/1.1 302 \r\n");
+            dos.writeBytes("Location: " + redirectpath+ "\r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: 0\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void responseByContentType(DataOutputStream dos,byte[] body, String contenttype){
+        switch (contenttype) { //content type에 따른 response
+            case "html":
+                response200Header(dos, body.length,"text/html;charset=utf-8");
+                responseBody(dos, body);
+                break;
+            case "css":
+                response200Header(dos, body.length,"text/css;charset=utf-8");
+                responseBody(dos, body);
+                break;
+            case "js":
+                response200Header(dos, body.length,"text/javascript;charset=utf-8");
+                responseBody(dos, body);
+                break;
+            case "ico":
+                response200Header(dos, body.length,"image/x-icon");
+                responseBody(dos, body);
+                break;
+            case "png":
+                response200Header(dos, body.length,"image/png");
+                responseBody(dos, body);
+                break;
+            case "jpg":
+                response200Header(dos, body.length,"image/jpeg");
+                responseBody(dos, body);
+                break;
+            case "svg":
+                response200Header(dos, body.length,"image/svg+xml");
+                responseBody(dos,body);
+                break;
+            default:
+                response405Header(dos);
+                break;
         }
     }
 
@@ -96,6 +144,14 @@ public class RequestHandler implements Runnable {
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void response404Header(DataOutputStream dos) {
+        try{
+            dos.writeBytes("HTTP/1.1 404 Not Found\r\n");
+        } catch (IOException e){
             logger.error(e.getMessage());
         }
     }
