f55618843d48fb5a6dd8a7bbf6bc34ae0571f3c4
violet-mj <violet_prog@naver.com>
2024-07-02 12:10:23 +0900

    - Request 클래스 추가하여 http header 파싱 - 정적 파일 반환 구현 - JAVA Thread에서 Concurrent 패키지로 변경 - README 수정 (Concurrent 패키지 학습)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,15 +1,17 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class RequestHandler implements Runnable {
+public class RequestHandler implements Callable<String> {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
@@ -18,20 +20,49 @@ public class RequestHandler implements Runnable {
         this.connection = connectionSocket;
     }
 
-    public void run() {
+    public String call() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-  	    // test1
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            Request request = new Request(br);
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "<h1>Hello World</h1>".getBytes();
+            FileInputStream fis = new FileInputStream("src/main/resources/static" + request.getPath());
+            byte[] body = fis.readAllBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+
+        return "hello";
+    }
+
+    private static Map<String, String> clientHeaderParser(InputStream in) throws IOException {
+        Map<String, String> httpHeaders = new ConcurrentHashMap<>();
+//        for(String line: headerLines) {
+//            StringTokenizer headerToken = new StringTokenizer(line, ": ");
+//            String headerName = headerToken.nextToken();
+//            String headerValue = headerToken.nextToken();
+//            httpHeaders.put(headerName, headerValue);
+//        }
+        return httpHeaders;
+    }
+
+
+
+    private String readString(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            byteArrayOutputStream.write(buffer, 0, bytesRead);
+            if (inputStream.available() == 0) {
+                break;
+            }
+        }
+        return new String(byteArrayOutputStream.toByteArray(), StandardCharsets.UTF_8);
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
