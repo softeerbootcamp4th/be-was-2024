9eadf2ed0c9eff9df54910b68c55ae3ea0e4bc24
win-luck <chaso1495@naver.com>
2024-07-02 18:17:57 +0900

    [feat] HttpRequest 객체 생성 및 다양한 응답 Content Type 지원

---
+++ src/main/java/util/HttpRequestObject.java
@@ -0,0 +1,30 @@
+package util;
+
+public class HttpRequestObject {
+
+    private final String requestMethod;
+    private final String requestURI;
+    private final String httpVersion;
+
+    private HttpRequestObject(String requestMethod, String requestURI, String httpVersion){
+        this.requestMethod = requestMethod;
+        this.requestURI = requestURI;
+        this.httpVersion = httpVersion;
+    }
+
+    public static HttpRequestObject from(String[] requestLineElements) {
+        return new HttpRequestObject(requestLineElements[0], requestLineElements[1], requestLineElements[2]);
+    }
+
+    public String getRequestMethod() {
+        return requestMethod;
+    }
+
+    public String getRequestURI() {
+        return requestURI;
+    }
+
+    public String getHttpVersion() {
+        return httpVersion;
+    }
+}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,6 +5,7 @@ import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestObject;
 import util.HttpRequestParser;
 
 public class RequestHandler implements Runnable {
@@ -22,32 +23,50 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            // 요청 Header 출력
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String line = br.readLine(); // Request Line (ex: "GET /index.html HTTP/1.1")
-            String uri = HttpRequestParser.parseRequestURI(line);
+            String[] requestLineElements = HttpRequestParser.parseRequestLine(line);
             while(!line.isEmpty()) {
                 logger.debug("header: {}", line);
                 line = br.readLine();
             }
 
+            // HttpRequestObject 생성
+            HttpRequestObject httpRequestObject = HttpRequestObject.from(requestLineElements);
+
+            // 데이터 입출력 및 응답
             DataOutputStream dos = new DataOutputStream(out);
-            File file = new File(BASE_PATH + uri);
+            File file = new File(BASE_PATH + httpRequestObject.getRequestURI());
             int lengthOfBodyContent = (int) file.length();
             byte[] body = new byte[lengthOfBodyContent];
             try (FileInputStream fis = new FileInputStream(file); BufferedInputStream bis = new BufferedInputStream(fis)) {
                 bis.read(body);
             }
-            response200Header(dos, body.length);
+            response200Header(dos, body.length, httpRequestObject.getRequestURI().split("\\.")[1]);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private String getContentType(String extension) {
+        return switch (extension) {
+            case "html" -> "text/html;charset=utf-8";
+            case "css" -> "text/css";
+            case "js" -> "application/javascript";
+            case "ico" -> "image/x-icon";
+            case "png" -> "image/png";
+            case "jpg" -> "image/jpeg";
+            case "svg" -> "image/svg+xml";
+            default -> "text/plain";
+        };
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String extension) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: " + getContentType(extension) + ";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
