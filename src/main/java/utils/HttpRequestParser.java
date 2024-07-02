9ca02b7470a6650dca7ea141c22b1a359b83d492
wjddn2165 <qowjddn2165@gmail.com>
2024-07-02 15:35:53 +0900

    HttpRequestParser 클래스에 확장자를 검증하고 추출하는 getExtension() 메서드 추가

--- src/main/java/utils/HttpRequestParser.java
+++ src/main/java/utils/HttpRequestParser.java
@@ -2,17 +2,23 @@ package utils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HttpRequestParser {
     private final String startLine;
     private final Map<String, String> headers;
-    public HttpRequestParser(BufferedReader br) throws IOException {
+    public HttpRequestParser(InputStream inputStream) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+
         startLine = br.readLine();
         headers = new HashMap<>();
 
-        String readLine = null;
+        String readLine;
         while (!(readLine = br.readLine()).isEmpty()) {
             String[] keyValue = readLine.split(": ");
             headers.put(keyValue[0], keyValue[1]);
@@ -25,5 +31,20 @@ public class HttpRequestParser {
 
     public Map<String, String> getHeaders() {
         return headers;
+    }
+
+    public String getExtension() {
+        String url = getUrl();
+        String regex = "\\.([a-z]+)$";
+
+        Pattern compile = Pattern.compile(regex);
+        Matcher matcher = compile.matcher(url);
+
+        // 파일 확장자를 가진 url인지 검증
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+
+        return null;
     }
 }
