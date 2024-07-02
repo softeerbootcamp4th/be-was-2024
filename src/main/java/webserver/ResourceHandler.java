f90925efd9805d9b1d1e096d58ef500972d56795
DrRivaski <leesm990708@gmail.com>
2024-07-02 15:33:19 +0900

    [mod] ResourceHandler 파일 접근 속도 개선 - 기존의 FileInputStream을 BufferedInputStream로 변경하여 파일 접근 속도 개선

--- src/main/java/webserver/ResourceHandler.java
+++ src/main/java/webserver/ResourceHandler.java
@@ -1,16 +1,21 @@
 package webserver;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class ResourceHandler {
     public byte[] getByteArray(String url) throws IOException {
+        // 파일
         File html = new File("src/main/resources/static" + url);
         byte[] body = new byte[(int) html.length()];
 
-        try (FileInputStream fileInputStream = new FileInputStream(html)) {
-            fileInputStream.read(body);
+        // 얘는 한글자씩 가져오는 방법임
+        // 버퍼를 사용하면 빠르게 가져올 수 있음
+        try (FileInputStream fileInputStream = new FileInputStream(html);
+             BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);) {
+            bufferedInputStream.read(body);
         }
 
         return body;
