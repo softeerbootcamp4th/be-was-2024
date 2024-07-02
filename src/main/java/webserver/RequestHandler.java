583e46f968cd02bdae5438324c4ef19df3a2ad60
eckrin <bsh51515@naver.com>
2024-07-02 10:42:55 +0900

    fix: week1 1단계 - 기능 요구사항 반영
    - uri 경로에 따라서 정적 페이지 응답
    - HTTP 요청을 Logger를 통해 Debug
    - ExecutorService를 통해 쓰레드풀 관리
    - 유지보수에 좋은 코드 만들기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,17 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Scanner;
 
+import common.WebUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private final String dirPath = "/Users/eckrin/study/be-was-2024/src/main/resources/static";
 
     private Socket connection;
 
@@ -18,14 +19,31 @@ public class RequestHandler implements Runnable {
         this.connection = connectionSocket;
     }
 
+    @Override
     public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String str, path = "";
+
+            while(!(str = br.readLine()).isEmpty()) {
+                String[] headerLine = str.split(" ");
+                if(WebUtils.isWebRequest(headerLine[0])) { // request uri path 추출하기
+                    path = headerLine[1];
+                }
+                logger.debug("{}", str);
+            }
+
+            // read file
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "<h1>Hello World</h1>".getBytes();
+            File file = new File(dirPath + path);
+            byte[] body = new byte[(int)file.length()];
+
+            try(FileInputStream fis = new FileInputStream(file)) {
+                fis.read(body);
+            }
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,7 +1,10 @@
 package webserver;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +12,7 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int THREAD_POOL_SIZE = 200; // 톰캣(Tomcat 9 기준)의 default max pool size는 200
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -18,16 +22,21 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
+        ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
-            // 클라이언트가 연결될때까지 대기한다.
+            // 클라이언트가 연결될 때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                threadPool.execute(new RequestHandler(connection));
             }
+        } catch (IOException e) {
+            logger.error("Server Start Error: " + e.getMessage());
+        } finally {
+            threadPool.shutdown();
         }
     }
 }
