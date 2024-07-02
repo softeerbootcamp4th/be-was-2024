f4ee29c600e394f8d71a286f2db4f6456bb3d4d6
wjddn2165 <qowjddn2165@gmail.com>
2024-07-02 11:31:14 +0900

    기존 쓰레드를 직접 생성하는 방식에서 쓰레드 풀 방식으로 변경

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -6,6 +6,10 @@ import java.net.Socket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
@@ -22,11 +26,21 @@ public class WebServer {
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
+            // 스레드 풀 생성
+            ExecutorService executorService = Executors.newFixedThreadPool(10);
+
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+
+
+                RequestHandler requestHandler = new RequestHandler(connection);
+//                Thread thread = new Thread(new RequestHandler(connection));
+//                thread.start();
+
+                executorService.submit(requestHandler);
+
+//                executorService.shutdown();
             }
         }
     }
