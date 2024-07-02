97e2b779a72e948438b7bac7e25fb8d1afd7c242
win-luck <chaso1495@naver.com>
2024-07-02 11:19:45 +0900

    [feat] Thread Concurrent 기반으로 개편

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,8 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,10 +26,15 @@ public class WebServer {
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
+            ExecutorService executor = Executors.newCachedThreadPool();
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                try {
+                   executor.execute(new RequestHandler(connection));
+                } catch (Exception e) {
+                    logger.error(e.getMessage());
+                }
             }
+            executor.shutdown();
         }
     }
 }
