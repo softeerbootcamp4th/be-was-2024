d8f465f4d8fef658cfef1567d503168fe4408a06
k000927 <kimsungjin927@gmail.com>
2024-07-02 10:40:42 +0900

    index.html 응답

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,8 +2,6 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,28 +16,42 @@ public class RequestHandler implements Runnable {
     }
 
     public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
 
-            InputStreamReader isr = new InputStreamReader(in, StandardCharsets.UTF_8);
+            InputStreamReader isr = new InputStreamReader(in, "UTF-8");
             BufferedReader br = new BufferedReader(isr);
 
             String line = br.readLine();
             logger.debug("request = " + line);
             String[] tokens = line.split(" ");
-
             String url = tokens[1];
-            if(url.equals("/")){
+
+            if (url.equals("/")) {
                 byte[] body = "<h1>Hello World</h1>".getBytes();
                 response200Header(dos, body.length);
                 responseBody(dos, body);
 
             }
-            byte[] body = Files.readAllBytes(new File("./src/main/resources/static" + url).toPath());
+
+            String path = "./src/main/resources/static" + url;
+            String str = "";
+            byte[] body = new byte[0];
+            try {
+                br = new BufferedReader(new FileReader(path));
+                String fileLine = br.readLine();
+
+                while (fileLine != null) {
+                    str += fileLine;
+                    fileLine = br.readLine();
+                }
+                body = str.getBytes();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
             while (line.isEmpty()) {
                 logger.debug("request = " + line);
