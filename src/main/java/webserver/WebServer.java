4eefc46b7af5bd89d87971677e92ccbceead882f
luna156 <v010309@naver.com>
2024-07-02 16:00:58 +0900

    week1-task3 commit 2 (database 추가)
    WebServer.java
    초기 실행시에 데이터베이스를 초기화시키는 루틴 추가
    user를 추가하는 method 추가

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -3,6 +3,8 @@ package webserver;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import db.Database;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +12,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class WebServer {
+    private static final Database database = new Database(); // DB 초기화
+
+    private static ExecutorService executor = Executors.newFixedThreadPool(10); // thread 갯수 제한을 위한 thread pool
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
-        ExecutorService executor = Executors.newFixedThreadPool(10); // thread 갯수 제한을 위한 thread pool
 
         if (args == null || args.length == 0) {
             port = DEFAULT_PORT;
@@ -33,5 +37,10 @@ public class WebServer {
                 executor.submit(new RequestHandler(connection)); // 요청이 올 시에 executor queue에 작업 추가
             }
         }
+    }
+
+    public static void addUser(String id, String username, String password, String email) {
+        database.addUser(new User(id, username, password, email));
+        logger.info("user {} added.", id);
     }
 }
