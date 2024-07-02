6b32e268194d22453eb349ec01d8c1864798a5e3
DrRivaski <leesm990708@gmail.com>
2024-07-02 16:01:58 +0900

    [mod] Thread를 Concurrent 패키지를 사용하도록 변경 - ExecutorService와 Executors.newCachedThreadPool()을 이용하여 스레드 풀 생성 - execute() 메소드를 이용하여 RequestHandler 실행 - isInterrupted() 메소드를 이용하여 인터럽트에 대한 예외 처리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -25,28 +25,30 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 
-            // InputStream을 BufferedReader로 변환
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            while (!Thread.currentThread().isInterrupted()) {
+                // InputStream을 BufferedReader로 변환
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
-            // 읽어들인 InputStream 모두 출력
-            String line; // 읽어들일 라인
-            String url = ""; // 요청 파일
-            while (!(line = br.readLine()).isEmpty()) {
-                // 요청이 GET일 경우
-                if (urlParser.getHttpMethod(line).equals("GET")) {
-                    logger.debug(line);
-                    // 요청 파일 받아오기
-                    url = urlParser.getGetURL(line);
+                // 읽어들인 InputStream 모두 출력
+                String line; // 읽어들일 라인
+                String url = ""; // 요청 파일
+                while (!(line = br.readLine()).isEmpty()) {
+                    // 요청이 GET일 경우
+                    if (urlParser.getHttpMethod(line).equals("GET")) {
+                        logger.debug(line);
+                        // 요청 파일 받아오기
+                        url = urlParser.getGetURL(line);
+                    }
                 }
+
+                DataOutputStream dos = new DataOutputStream(out);
+
+                // url로부터 html파일을 byte array로 읽어오기
+                byte[] body = resourceHandler.getByteArray(url);
+
+                response200Header(dos, body.length);
+                responseBody(dos, body);
             }
-
-            DataOutputStream dos = new DataOutputStream(out);
-
-            // url로부터 html파일을 byte array로 읽어오기
-            byte[] body = resourceHandler.getByteArray(url);
-
-            response200Header(dos, body.length);
-            responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
