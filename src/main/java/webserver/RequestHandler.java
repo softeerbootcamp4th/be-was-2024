ababde3f06523a09b2c9bce451819ed48c733292
admin <admin@adminui-MacBookPro.local>
2024-07-02 09:56:33 +0900

    step1

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,9 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,15 +22,29 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            String line = br.readLine();
+            logger.debug("request line : {} ",line);
+            String url = parseUrl(line);
+            System.out.println(url);
+            while(!line.equals(""))
+            {
+                line = br.readLine();
+                logger.debug("request line : {}",line);
+            }
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "<h1>Hello World</h1>".getBytes();
-            response200Header(dos, body.length);
+            byte[] body = Files.readAllBytes(new File("src/main/resources/static/"+url).toPath());
+
+            response200Header(dos,body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
+
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
@@ -51,5 +64,13 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private String parseUrl(String header)
+    {
+        String[] tokens = header.split("/");
+        String[] ntokens= tokens[1].split(" ");
+
+        return ntokens[0];
     }
 }
