7589d296e1c4cc0f14a69a79f920273865dfe98d
admin <admin@admins-MacBook-Pro.local>
2024-07-02 15:35:39 +0900

    정적인 html 파일 응답 구현 : 요청헤더에서 URL을 추출하여 responseByURL() 메서드에 인자로 넘겨주어 응답하도록 구현하였다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,7 +2,9 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.io.File;
 
+import http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,34 +23,66 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
-
             String requestURL = "";
+            boolean isFirstLine = true;
 
             String line;
             StringBuilder sb = new StringBuilder("\n");
             while (!(line = bufferedReader.readLine()).isEmpty()){ //null check는 broken pipe 에러를 발생시킨다.
-                sb.append(line + "\n");
+                if(isFirstLine){
+                    String[] tokens = line.split(" ");
+                    requestURL = tokens[1];
+                    isFirstLine = false;
+                }
+                sb.append(line).append("\n");
             }
             logger.debug(sb.toString());
 
-            DataOutputStream dos = new DataOutputStream(out);
+            responseByURL(new DataOutputStream(out), requestURL);
 
-            byte[] body = "<h1>Hello World</h1>".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void responseByURL(DataOutputStream dos, String requestURL){
+        File file = new File("src/main/resources/static" + requestURL);
+
+        if(!file.exists()){
+            byte[] body = "<h1>Page Not Found!</h1>".getBytes();
+            responseHeader(dos, body.length, HttpStatus.SC_NOT_FOUND);
+            responseBody(dos, body);
+            return;
+        }
+
+        try(FileInputStream fis = new FileInputStream(file)){
+            byte[] body = fis.readAllBytes();
+            responseHeader(dos, body.length, HttpStatus.SC_OK);
+            responseBody(dos, body);
+        } catch (IOException e){
+            logger.error(e.getMessage());
+        }
+
+    }
+
+    private void responseHeader(DataOutputStream dos, int lengthOfBodyContent, int statusCode){
         try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            StringBuilder statusInfo = new StringBuilder()
+                    .append(statusCode)
+                    .append(" ")
+                    .append(HttpStatus.getStautusString(statusCode))
+                    .append("\r\n");
+
+            dos.writeBytes("HTTP/1.1  "+ statusInfo.toString());
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             logger.error(e.getMessage());
+        } catch (IllegalStateException ie){
+            byte[] body = "<h1>Server Error</h1>".getBytes();
+            responseHeader(dos, body.length, HttpStatus.SC_INTERNAL_SERVER_ERROR);
+            responseBody(dos, body);
         }
     }
 
