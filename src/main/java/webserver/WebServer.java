9a85a1e69d381f97902c078648d00bf30869e4a7
win-luck <chaso1495@naver.com>
2024-07-02 15:50:56 +0900

    [fix] ExecutorService ThreadPool 고정 크기 생성으로 수정

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int DEFAULT_THREAD_POOL_SIZE = 200;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -21,7 +22,7 @@ public class WebServer {
         }
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
-        ExecutorService executor = Executors.newCachedThreadPool();
+        ExecutorService executor = Executors.newFixedThreadPool(DEFAULT_THREAD_POOL_SIZE);
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
