da1a77c3c0b71443eb6b67ee300758e0bc5a0cc6
win-luck <chaso1495@naver.com>
2024-07-02 12:21:48 +0900

    [refactor] RequestLine 추출용 함수 구현 및 BASE_PATH 상수 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String BASE_PATH = "src/main/resources/static";
 
     private Socket connection;
 
@@ -21,17 +22,17 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-            String line = br.readLine();
-            String url = line.split(" ")[1];
+            String line = br.readLine(); // Request Line (ex: "GET /index.html HTTP/1.1")
+            String[] requestLine = getRequestLineElements(line);
             while(!line.isEmpty()) {
                 logger.debug("header: {}", line);
                 line = br.readLine();
             }
 
             DataOutputStream dos = new DataOutputStream(out);
-            File file = new File("src/main/resources/static" + url);
+            File file = new File(BASE_PATH + requestLine[1]);
             byte[] body = new byte[(int) file.length()];
-            try(FileInputStream fis = new FileInputStream(file); BufferedInputStream bis = new BufferedInputStream(fis)) {
+            try (FileInputStream fis = new FileInputStream(file); BufferedInputStream bis = new BufferedInputStream(fis)) {
                 bis.read(body);
             }
             response200Header(dos, body.length);
@@ -39,6 +40,13 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private String[] getRequestLineElements(String requestLine){
+        // 0: RequestType (ex: GET)
+        // 1: RequestURI (ex: /index.html)
+        // 2: HTTP version (ex: HTTP/1.1
+        return requestLine.split(" ");
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
