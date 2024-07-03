f0e9e91a28c851dd000d144df5a572f1ed81f9f9
DrRivaski <leesm990708@gmail.com>
2024-07-03 16:52:36 +0900

    [mod] content-type을 테스트하기 위한 테스트 코드 수정 - 매개변수로 보내는 url 수정

--- src/test/java/webserver/TYPETest.java
+++ src/test/java/webserver/TYPETest.java
@@ -1,15 +1,17 @@
 package webserver;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TYPETest {
     @Test
+    @DisplayName("resource handler get mime 테스트")
     void test() {
         ResourceHandler resourceHandler = new ResourceHandler();
 
-        String contentType = resourceHandler.getContentType("GET /back.ico HTTP/1.0");
+        String contentType = resourceHandler.getContentType("/back.ico");
         assertEquals("image/x-icon", contentType);
     }
 }
