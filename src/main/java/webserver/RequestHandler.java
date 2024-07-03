ea2cfcfaef34106b62e6e44521d9cb0dbb9778da
win-luck <chaso1495@naver.com>
2024-07-03 13:29:23 +0900

    [fix] 파일 입출력 기능 별도 클래스로 분리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,12 +5,13 @@ import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.FileUtil;
 import util.HttpRequestObject;
 import util.HttpRequestParser;
+import util.ContentType;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-    private static final String BASE_PATH = "src/main/resources/static";
 
     private Socket connection;
 
@@ -22,9 +23,9 @@ public class RequestHandler implements Runnable {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream();
+             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"))) {
             // 요청 Header 출력
-            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String line = br.readLine(); // Request Line (ex: "GET /index.html HTTP/1.1")
             String[] requestLineElements = HttpRequestParser.parseRequestLine(line);
             while(!line.isEmpty()) {
@@ -37,12 +38,7 @@ public class RequestHandler implements Runnable {
 
             // 데이터 입출력 및 응답
             DataOutputStream dos = new DataOutputStream(out);
-            File file = new File(BASE_PATH + httpRequestObject.getRequestURI());
-            int lengthOfBodyContent = (int) file.length();
-            byte[] body = new byte[lengthOfBodyContent];
-            try (FileInputStream fis = new FileInputStream(file); BufferedInputStream bis = new BufferedInputStream(fis)) {
-                bis.read(body);
-            }
+            byte[] body = FileUtil.readBytesFromFile(httpRequestObject.getRequestURI());
             response200Header(dos, body.length, httpRequestObject.getRequestURI().split("\\.")[1]);
             responseBody(dos, body);
         } catch (IOException e) {
@@ -50,23 +46,10 @@ public class RequestHandler implements Runnable {
         }
     }
 
-    private String getContentType(String extension) {
-        return switch (extension) {
-            case "html" -> "text/html;charset=utf-8";
-            case "css" -> "text/css";
-            case "js" -> "application/javascript";
-            case "ico" -> "image/x-icon";
-            case "png" -> "image/png";
-            case "jpg" -> "image/jpeg";
-            case "svg" -> "image/svg+xml";
-            default -> "text/plain";
-        };
-    }
-
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String extension) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: " + getContentType(extension) + ";charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: " + ContentType.getType(extension) + "\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
