28753c5fa3efee411aa7fe0e1db5d7dbfb6b44be
k000927 <kimsungjin927@gmail.com>
2024-07-03 16:48:45 +0900

    feature: Database 싱글톤 생성 가능하도록 작성

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -7,14 +7,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Database {
-    private static Database instance;
     private static Map<String, User> users = new HashMap<>();
+    private static Database instance;
 
     private Database() {
     }
 
-    public static Database getInstance(){
-        if(users == null) instance = new Database();
+    public static Database getInstance() {
+        if (instance == null) {
+            synchronized (Database.class) {
+                if (instance == null) {
+                    instance = new Database();
+                }
+            }
+        }
         return instance;
     }
 
