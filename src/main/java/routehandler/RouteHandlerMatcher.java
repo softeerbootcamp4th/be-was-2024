cb8c08d6854c31684a4c3eedcf43b84759f1bc77
blaxsior <heejun2798@gmail.com>
2024-07-03 16:31:00 +0900

    feat: RouteHandler / RouterHandlerMatcher 클래스 추가
    - routehandler: 차후 static이 아닌 일반적인 요청에도 대응하기 쉽도록 특정 종류의 요청처리단을 추상화한 IRouteHandler 인터페이스 생성
    - StaticResourceHandler: static 경로를 다루는 핸들러 구현
    - RouteHandlerMatcher: 경로 조건을 받고, 대응되는 객체를 반환하는 매칭 객체

---
+++ src/main/java/routehandler/RouteHandlerMatcher.java
@@ -0,0 +1,30 @@
+package routehandler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RouteHandlerMatcher {
+    List<IRouteHandler> routeHandlers;
+
+    public RouteHandlerMatcher() {
+        this.routeHandlers = new ArrayList<>();
+    }
+
+    public RouteHandlerMatcher(IRouteHandler... routeHandlers) {
+        this();
+        this.routeHandlers.addAll(Arrays.asList(routeHandlers));
+    }
+
+    public IRouteHandler getMatchedRouteHandler(String url) {
+        IRouteHandler routeHandler = null;
+        for (IRouteHandler handler : routeHandlers) {
+            if(!handler.canMatch(url)) continue;
+
+            routeHandler = handler;
+            break;
+        }
+
+        return routeHandler;
+    }
+}
