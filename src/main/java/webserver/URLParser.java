a2e0d229d0b0d23f959e9969a7c0c4d440f66eea
DrRivaski <leesm990708@gmail.com>
2024-07-03 18:10:57 +0900

    [mod] URLParser에서 url 가져오는 방식 수정 - /registration으로 요청 시 /registration/index.html로 이동할 수 있도록 먼저 요청 url이 파일인지 디렉토리인지 판단한 후 반환하도록 수정

--- src/main/java/webserver/URLParser.java
+++ src/main/java/webserver/URLParser.java
@@ -1,8 +1,17 @@
 package webserver;
 
+import java.io.File;
+
 public class URLParser {
     public String getURL(String line) {
-        return line.split(" ")[1];
+        String url = line.split(" ")[1];
+        File testFile = new File("src/main/resources/static" + url);
+
+        if (testFile.isDirectory()) {
+            return url + "/index.html";
+        } else {
+            return url;
+        }
     }
 
     public String getHttpMethod(String line) {
