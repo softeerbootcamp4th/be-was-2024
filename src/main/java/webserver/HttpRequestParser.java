2d536f0cf664270039081e99019746ecb9aa66b0
min9805 <min1925k@gmail.com>
2024-07-03 15:21:34 +0900

    refactor: Request Parsing 부분 추출
    - RequestHandler 의 Parsing 부분 추출 후 메서드화

--- src/main/java/webserver/HttpRequestParser.java
+++ src/main/java/webserver/HttpRequestParser.java
@@ -1,10 +1,40 @@
 package webserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HttpRequestParser {
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequestParser.class);
     private static final HttpRequestParser instance = new HttpRequestParser();
 
     public static HttpRequestParser getInstance() {
         return instance;
+    }
+
+    public Map<String, String> parseRequestHeaders(BufferedReader br) throws IOException {
+        String line;
+        StringBuilder log = new StringBuilder();
+        Map<String, String> headers = new HashMap<>();
+
+        // 요청 메시지를 한 줄씩 읽어 StringBuilder 에 추가
+        while ((line = br.readLine()) != null && !line.isEmpty()) {
+            log.append(line).append("\n");
+            int index = line.indexOf(':');
+            if (index > 0) {
+                String headerName = line.substring(0, index).trim();
+                String headerValue = line.substring(index + 1).trim();
+                headers.put(headerName, headerValue);
+            }
+        }
+
+        logger.debug("Http Request Header : {} ", log);
+
+        return headers;
     }
 
     public String parseRequestURI(String requestLine) {
@@ -22,16 +52,16 @@ public class HttpRequestParser {
     public String parseRequestContentType(String requestLine) {
         String[] pathParts = requestLine.split("/");
         if (pathParts.length < 1) {
-            return null;
+            return ContentType.html.getContentType();
         }
         String lastPathPart = pathParts[pathParts.length - 1];
 
         // 파일 이름에서 확장자 추출
         int lastDotIndex = lastPathPart.lastIndexOf('.');
         if (lastDotIndex != -1) {
-            return lastPathPart.substring(lastDotIndex + 1);
+            return ContentType.valueOf(lastPathPart.substring(lastDotIndex + 1)).getContentType();
         } else {
-            return null;
+            return ContentType.html.getContentType();
         }
     }
 }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.HashMap;
 import java.util.Map;
 
 public class RequestHandler implements Runnable {
@@ -26,41 +25,26 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // Request
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            StringBuilder requestBuilder = new StringBuilder();
-            String line;
-            Map<String, String> headers = new HashMap<>();
 
-            line = br.readLine();
-            String uri = httpRequestParser.parseRequestURI(line);
-            String extension = httpRequestParser.parseRequestContentType(uri);
+            // First Line
+            String firstLine = br.readLine();
+            logger.debug("HTTP Request First Line: {}", firstLine);
 
+            // Headers
+            Map<String, String> headers = httpRequestParser.parseRequestHeaders(br);
 
-            String contentType = ContentType.html.getContentType();
-            if (extension != null) {
-                contentType = ContentType.valueOf(extension).getContentType();
-            }
+            // Path 및 Content-Type 지정
+            String uri = httpRequestParser.parseRequestURI(firstLine);
+            String contentType = httpRequestParser.parseRequestContentType(uri);
 
-            requestBuilder.append(line).append("\n");
-
-            // 요청 메시지를 한 줄씩 읽어 StringBuilder 에 추가
-            while ((line = br.readLine()) != null && !line.isEmpty()) {
-                requestBuilder.append(line).append("\n");
-                int index = line.indexOf(':');
-                if (index > 0) {
-                    String headerName = line.substring(0, index).trim();
-                    String headerValue = line.substring(index + 1).trim();
-                    headers.put(headerName, headerValue);
-                }
-            }
-
-            logger.debug("HTTP Request : {}", requestBuilder);
 
             // Response
-
             DataOutputStream dos = new DataOutputStream(out);
 
+            // File reader
             byte[] body = fileContentReader.readStaticResource(uri);
 
+            // Response
             response200Header(dos, contentType, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
