6238bb32fa626ec8acd12a012a7be66d4174e684
ysndy <ysndy@naver.com>
2024-07-04 17:16:54 +0900

    fix: 응답의 책임을 RequestHandler 에서 ResponseHandler 로 위임

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,14 +3,10 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 
-import db.Database;
-import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.request.HttpRequest;
 import webserver.request.Path;
-import webserver.request.Parameter;
-import webserver.response.HttpResponse;
 import webserver.response.ResponseHandler;
 
 import static util.Utils.*;
@@ -37,37 +33,11 @@ public class RequestHandler implements Runnable {
             Path path = httpRequest.getPath();
 
             if(httpRequest.getPath().isStatic()){
-
-                byte[] body = getFile(path.get());
-                HttpResponse response = new HttpResponse(200, body);
-                response.addHeader("Content-Type", getContentType(path.getExtension())+";charset=utf-8");
-                response.addHeader("Content-Length", String.valueOf(body.length));
-                ResponseHandler.response(out, response);
-            }else {
-                switch(path.get()){
-                    case "/registration":
-                        byte[] body = "".getBytes();
-                        HttpResponse response = new HttpResponse(302, body);
-                        response.addHeader("Content-Length", String.valueOf(body.length));
-                        response.addHeader("Location", "/registration/index.html");
-
-                        ResponseHandler.response(out, response);
-                        break;
-
-                    case "/create":
-                        Parameter parameter = path.getParameter().get();
-                        User user = new User(
-                                parameter.get("userId"),
-                                parameter.get("password"),
-                                parameter.get("name"),
-                                parameter.get("email")
-                        );
-                        Database.addUser(user);
-                        System.out.println(user);
-                        break;
-                }
+                ResponseHandler.responseStaticContents(out, path);
+                return;
             }
 
+            ResponseHandler.responseDynamicContents(out, path);
 
         } catch (IOException e) {
             logger.error(e.getMessage());
--- src/main/java/webserver/response/ResponseHandler.java
+++ src/main/java/webserver/response/ResponseHandler.java
@@ -1,16 +1,55 @@
 package webserver.response;
 
+import db.Database;
+import model.User;
+import webserver.request.Parameter;
 import webserver.request.Path;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import static util.Utils.getContentType;
+import static util.Utils.getFile;
+
 public class ResponseHandler {
 
     public static void response(OutputStream outputStream, HttpResponse httpResponse) throws IOException {
         DataOutputStream dos = new DataOutputStream(outputStream);
         dos.write(httpResponse.toByte());
+    }
+
+    public static void responseStaticContents(OutputStream out, Path path) throws IOException {
+        byte[] body = getFile(path.get());
+        HttpResponse response = new HttpResponse(200, body);
+        response.addHeader("Content-Type", getContentType(path.getExtension())+";charset=utf-8");
+        response.addHeader("Content-Length", String.valueOf(body.length));
+        ResponseHandler.response(out, response);
+    }
+
+    public static void responseDynamicContents(OutputStream out, Path path) throws IOException {
+
+        switch(path.get()){
+            case "/registration":
+                byte[] body = "".getBytes();
+                HttpResponse response = new HttpResponse(303, body);
+                response.addHeader("Content-Length", String.valueOf(body.length));
+                response.addHeader("Location", "/registration/index.html");
+                ResponseHandler.response(out, response);
+                break;
+
+            case "/create":
+                Parameter parameter = path.getParameter().get();
+                User user = new User(
+                        parameter.get("userId"),
+                        parameter.get("password"),
+                        parameter.get("name"),
+                        parameter.get("email")
+                );
+                Database.addUser(user);
+                break;
+        }
+
     }
 
 }
