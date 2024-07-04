b09726aa0cf769dcf95e9d13bd0fb6b8ef7f019e
admin <admin@adminui-MacBookPro.local>
2024-07-04 18:30:08 +0900

    Week1 Task3 requestHandler Test 코드 리팩토링 완료

--- src/test/java/webserver/RequestHandlerTest.java
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -1,26 +1,30 @@
 package webserver;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.net.Socket;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class RequestHandlerTest {
 
-    private final String extensions = "css";
-
     private Socket connection;
 
-    public RequestHandler requestHandler = new RequestHandler(connection) ;
 
-    @DisplayName("match테스트")
+    @DisplayName("파싱되어 나온 확장자가 MIME 타입으로 잘 변환 되는지 테스트")
     @Test
     public void matchTest()
     {
-        Assertions.assertEquals("text/css",requestHandler.match(extensions));
+        //given
+        String extensions = "css";
+
+        //when
+        RequestHandler requestHandler = new RequestHandler(connection);
+
+        //then
+        assertThat(requestHandler.match(extensions)).isEqualTo("text/css");
     }
 
 }
