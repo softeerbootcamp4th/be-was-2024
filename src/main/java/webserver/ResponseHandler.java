a8e2e15a330a92131d28b9a736d9ee04fd09bd42
DrRivaski <leesm990708@gmail.com>
2024-07-04 12:19:18 +0900

    [mod] 로직은 LogicProcessor 클래스가 처리하도록 수정 - 쿼리스트링일 경우 logicString이 처리하도록 수정 - 쿼리스트링이 아닐 경우 기존과 같이 처리, 코드의 반복을 막기 위해 response라는 메소드 추가.

--- src/main/java/webserver/ResponseHandler.java
+++ src/main/java/webserver/ResponseHandler.java
@@ -11,19 +11,33 @@ public class ResponseHandler {
 
     private static final URLParser urlParser = new URLParser();
     private static final ResourceHandler resourceHandler = new ResourceHandler();
+    private static final LogicProcessor logicProcessor = new LogicProcessor();
 
     public void runGetResponse(String requestLine, DataOutputStream dos) throws IOException {
         String url = urlParser.getURL(requestLine);
 
+        if (urlParser.isQueryString(url)) { // 쿼리 스트링인 경우
+            // 데이터 파싱 및 저장 로직
+            // logicProcessor 이용
+
+            logicProcessor.createUser(url);
+
+            response("/index.html", dos);
+        } else {
+            response(url, dos);
+        }
+    }
+
+    public void runPostResponse() {
+
+    }
+
+    private void response(String url, DataOutputStream dos) throws IOException {
         // url로부터 html파일을 byte array로 읽어오기
         byte[] body = resourceHandler.getByteArray(url);
 
         response200Header(dos, body.length, resourceHandler.getContentType(url));
         responseBody(dos, body);
-    }
-
-    public void runPostResponse() {
-
     }
 
 
