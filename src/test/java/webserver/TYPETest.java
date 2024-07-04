dc7ce493975f5446640a1c7bc7a0d101e1a1ed5c
DrRivaski <leesm990708@gmail.com>
2024-07-04 15:28:13 +0900

    [mod] assertThat을 사용하도록 수정 - 기존의 jupiter에서 assertj를 사용하도록 수정 - give, when, then 표시

--- src/test/java/webserver/TYPETest.java
+++ src/test/java/webserver/TYPETest.java
@@ -1,17 +1,23 @@
 package webserver;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class TYPETest {
+
     @Test
     @DisplayName("resource handler get mime 테스트")
     void test() {
+        // given
         ResourceHandler resourceHandler = new ResourceHandler();
 
+        // when
         String contentType = resourceHandler.getContentType("/back.ico");
-        assertEquals("image/x-icon", contentType);
+
+        // then
+        assertThat(contentType).isEqualTo("image/x-icon");
     }
 }
