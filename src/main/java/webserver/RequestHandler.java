c7832936693d397ea65d0b66e55d0a85c4af3142
admin <admin@adminui-MacBookPro.local>
2024-07-04 20:30:59 +0900

    Week1 Task3 회원가입시 성공 알람창 뜨고 login화면으로 넘어가게 설정

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,6 +9,8 @@ import processor.UserProcessor;
 import util.FileDetection;
 import util.RequestObject;
 
+import javax.xml.crypto.Data;
+
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
@@ -34,12 +36,9 @@ public class RequestHandler implements Runnable {
                 line = br.readLine();
                 logger.debug("request Headers : {}",line);
             }
-
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
             frontRequest(dos,requestObject);
-
-
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
@@ -52,12 +51,20 @@ public class RequestHandler implements Runnable {
         if(method.equals("GET") && path.equals("/user/create"))//GET방식의 회원가입 일 시
         {
             UserProcessor.userCreate(requestObject);
+            responseAlert(dos,"로그인 성공");
             return ;
         }
 
-        // 파일 요청이 들어왔다면
-        byte[] body;
+        // 파일 요청이 들어왔다면 경로를 디렉토리인지 아닌지 판단해서 다시 설정해준다
         path = FileDetection.getPath(FileDetection.fixedPath + path) ;
+        staticFileHandler(dos,path);
+    }
+
+
+    //static html 파일은 여기서 다뤄준다
+    private void staticFileHandler(DataOutputStream dos,String path)
+    {
+        byte[] body;
         File fi = new File(path);
         try(FileInputStream fin = new FileInputStream(fi);
             BufferedInputStream bi = new BufferedInputStream(fin);)
@@ -71,7 +78,17 @@ public class RequestHandler implements Runnable {
         {
             logger.error(e.getMessage());
         }
+    }
 
+    private void response302Header(DataOutputStream dos,String loc)
+    {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: "+loc+"\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
     }
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent,String url) {
         try {
@@ -88,6 +105,21 @@ public class RequestHandler implements Runnable {
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
             dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void responseAlert(DataOutputStream dos, String content) {
+        try {
+            String body = "<html><head><script type='text/javascript'>alert('" + content + "');window.location='/login/index.html';</script></head></html>";
+            byte[] bodyBytes = body.getBytes("UTF-8");
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + bodyBytes.length + "\r\n");
+            dos.writeBytes("\r\n");
+            dos.write(bodyBytes);
             dos.flush();
         } catch (IOException e) {
             logger.error(e.getMessage());
