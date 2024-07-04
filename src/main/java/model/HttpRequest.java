151518e78ad779fdcbf2e29df1f61c4dd9f7e5ca
k000927 <kimsungjin927@gmail.com>
2024-07-04 18:00:29 +0900

    refactor: 안쓰는 메소드 삭제 (Getter들 삭제)

--- src/main/java/model/HttpRequest.java
+++ src/main/java/model/HttpRequest.java
@@ -1,11 +1,4 @@
 package model;
 
 public record HttpRequest(HttpMethod method, String url) {
-    public HttpMethod getMethod(){
-        return this.method;
-    }
-
-    public String getURL(){
-        return this.url;
-    }
 }
