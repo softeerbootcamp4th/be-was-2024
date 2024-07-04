3b25d99353dc95857793dcf1ac3576033b7bea91
blaxsior <heejun2798@gmail.com>
2024-07-04 18:22:27 +0900

    test: HttpResponseMessageBuildUtil 예외처리 추가 및 테스트

--- src/main/java/http/utils/HttpResponseMessageBuildUtil.java
+++ src/main/java/http/utils/HttpResponseMessageBuildUtil.java
@@ -10,12 +10,17 @@ public class HttpResponseMessageBuildUtil {
         StringBuilder builder = new StringBuilder();
 
         // startLine
+        String version = response.getVersion();
         HttpStatusType statusType = response.getStatusType();
+        // 둘 중 하나만 없어도 라인 작성 불가
+        if(version == null || statusType == null) throw new RuntimeException("required argument not exist");
+
         int code = statusType.getStatusCode();
         String message = statusType.getMessage();
         String startLine = response.getVersion() + " " + code + " " + message + "\r\n";
 
         builder.append(startLine);
+
         // header
         for(var entries: response.getHeaders().getHeaders().entrySet()) {
             String header = entries.getKey();
@@ -24,12 +29,13 @@ public class HttpResponseMessageBuildUtil {
             String headerLine = header + ": " + value + "\r\n";
             builder.append(headerLine);
         }
+
         // 빈 라인
         builder.append("\r\n");
 
         // body
         if(response.getBody() != null)
-            builder.append(new String(response.getBody(), StandardCharsets.UTF_8));
+            builder.append(new String(response.getBody()));
         return builder.toString().getBytes();
     }
 }
