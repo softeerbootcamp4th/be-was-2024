bfc4f7682f3db682aaf413f1b4ec2395ebb17fbe
DrRivaski <leesm990708@gmail.com>
2024-07-04 11:55:25 +0900

    [mod] URLParser에서 쿼리 스트링인지 판단할 수 있도록 수정 - 쿼리스트링에 "?"가 들어간다는 점을 이용하여 판단하도록 수정

--- src/main/java/webserver/URLParser.java
+++ src/main/java/webserver/URLParser.java
@@ -5,6 +5,16 @@ import java.io.File;
 public class URLParser {
     public String getURL(String line) {
         String url = line.split(" ")[1];
+
+        if (!url.contains("?")) { // 쿼리 스트링이 아닌 경우
+            return getStaticUrl(url);
+        } else { // 쿼리스트링인 경우
+            System.out.println("asdfasdfasdf");
+            return "/index.html";
+        }
+    }
+
+    private String getStaticUrl(String url) {
         File testFile = new File("src/main/resources/static" + url);
 
         if (testFile.isDirectory()) {
