23410fff6c075c3425a69827bbd7ce690cef66cf
DrRivaski <leesm990708@gmail.com>
2024-07-05 14:10:38 +0900

    [add] GET 메소드와 path별로 처리를 다르게 해주는 GetDistributor 추가 - Distributor interface를 상속함 - 외부에서는 process() 메소드만 호출하면 http method와 path에 따라 다르게 동작하도록 구현

---
+++ src/main/java/webserver/GetDistributor.java
@@ -0,0 +1,31 @@
+package webserver;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class GetDistributor implements Distributor{
+    Request request;
+    Response response;
+    LogicProcessor logicProcessor = new LogicProcessor();
+    protected GetDistributor(Request request, Response response) {
+        this.request = request;
+        this.response = response;
+    }
+
+    @Override
+    public void process(DataOutputStream dos) throws IOException {
+        if (request.isQueryString()) {
+            processQeury(dos);
+        } else {
+            response.response(request.getPath(), dos);
+        }
+    }
+
+    private void processQeury(DataOutputStream dos) throws IOException {
+        String path = request.getPath();
+        if (path.equals("/user/create")) {
+            logicProcessor.createUser(request);
+            response.redirect("/index.html", dos, 302);
+        }
+    }
+}
