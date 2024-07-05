9f1dc984aab05b4d7cda02ad7a7d34095deb075c
DrRivaski <leesm990708@gmail.com>
2024-07-05 11:16:51 +0900

    [mod] 리다이렉트 메소드 변경에 따른 수정 - 리다이렉트 메소드 호출 시 리다이렉트 코드 전달하도록 수정

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -30,64 +30,27 @@ public class RequestHandler implements Runnable {
                 BufferedReader br = new BufferedReader(new InputStreamReader(in));
                 DataOutputStream dos = new DataOutputStream(out);
 
-                // 읽어들인 InputStream 모두 출력
                 String requestLine = br.readLine();
-                Request request = new Request(requestLine);
+                Request request = Request.from(requestLine);
                 String method = request.getHttpMethod();
 
                 if (method.equals("GET")) {
                     if (request.isQueryString()) {
                         logicProcessor.createUser(request);
-                        response.redirect("/index.html", dos);
+                        response.redirect("/index.html", dos, 302);
                     } else {
-                        response.response(request.getStaticPath(), dos);
+                        response.response(request.getPath(), dos);
                     }
                 }
 
+                // 읽어들인 InputStream 모두 출력
                 String line;
                 while (!(line = br.readLine()).isEmpty()) {
                     logger.debug(line); // 읽어들인 라인 출력
                 }
-//                String line; // 읽어들일 라인
-//                String url = ""; // 요청 파일
-//                while (!(line = br.readLine()).isEmpty()) {
-//                    logger.debug(line); // 읽어들인 라인 출력
-//                    // 요청이 GET일 경우
-//                    if (urlParser.getHttpMethod(line).equals("GET")) {
-//                        // 요청 파일 받아오기
-//                        url = urlParser.getURL(line);
-//                        responseHandler.getResponse(url, dos);
-//                    }
-//                }
-
-                // url로부터 html파일을 byte array로 읽어오기
-//                byte[] body = resourceHandler.getByteArray(url);
-//
-//                response200Header(dos, body.length, resourceHandler.getContentType(url));
-//                responseBody(dos, body);
             }
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
-
-//    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
-//        try {
-//            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-//            dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8\r\n");
-//            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-//            dos.writeBytes("\r\n");
-//        } catch (IOException e) {
-//            logger.error(e.getMessage());
-//        }
-//    }
-//
-//    private void responseBody(DataOutputStream dos, byte[] body) {
-//        try {
-//            dos.write(body, 0, body.length);
-//            dos.flush();
-//        } catch (IOException e) {
-//            logger.error(e.getMessage());
-//        }
-//    }
 }
