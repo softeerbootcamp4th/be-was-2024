e380efc1a9505a52434f8e10a04c784a4963cf27
Jongseo Lee <jongseo12345@gmail.com>
2024-07-05 13:53:43 +0900

    add: config file
    상수 관리를 위한 config 클래스 생성

---
+++ src/main/java/config/Config.java
@@ -0,0 +1,8 @@
+package config;
+
+public class Config {
+    public static final String ROOT_DIRECTORY = "src/main/resources/static";
+    public static final String DEFAULT_PAGE = "/index.html";
+    public static final int DEFAULT_PORT = 8080;
+    public static final int THREAD_POOL_SIZE = Runtime.getRuntime().availableProcessors();
+}
