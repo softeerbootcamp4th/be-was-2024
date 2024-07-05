f2b0f791c6bf7ca8ba7c6c950cd1e7c074202b23
Jongseo Lee <jongseo12345@gmail.com>
2024-07-05 14:12:23 +0900

    fix: webserver
    socker connection만 받던 request handler부분에 다른 의존성 주입

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -5,13 +5,18 @@ import java.net.Socket;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 
+import config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.DynamicRequestHandler;
+import util.HttpRequestParser;
+import util.StaticRequestHandler;
+import util.UrlMapper;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
-    private static final int DEFAULT_PORT = 8080;
-    private static final int THREAD_POOL_SIZE = Runtime.getRuntime().availableProcessors();
+    private static final int DEFAULT_PORT = Config.DEFAULT_PORT;
+    private static final int THREAD_POOL_SIZE = Config.THREAD_POOL_SIZE;
     public static void main(String args[]) throws Exception {
         int port = 0;
         if (args == null || args.length == 0) {
@@ -31,9 +36,14 @@ public class WebServer {
 //                Thread thread = new Thread(new RequestHandler(connection));
 //                thread.start();
 //            }
+            HttpRequestParser requestParser = new HttpRequestParser();
+            UrlMapper urlMapper = new UrlMapper();
+            StaticRequestHandler staticRequestHandler = new StaticRequestHandler();
+            DynamicRequestHandler dynamicRequestHandler = new DynamicRequestHandler();
             while (true) {
                 Socket connection = listenSocket.accept();
-                executor.execute(new RequestHandler(connection));
+                executor.execute(new RequestHandler(connection, requestParser, urlMapper,
+                        staticRequestHandler, dynamicRequestHandler));
             }
         }
     }
