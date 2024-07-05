602ee8f21d4e3630c07095f106301d85fac3abec
admin <admin@admins-MacBook-Pro.local>
2024-07-05 10:36:18 +0900

    쿼리 파라미터를 조회했을때 찾을 수 없는 상황에서 발생하는 Exception 생성. 클라이언트의 잘못된 URL접근에 의한 예외이다

---
+++ src/main/java/exception/QueryParameterNotFoundException.java
@@ -0,0 +1,23 @@
+package exception;
+
+public class QueryParameterNotFoundException extends Exception {
+    // 기본 생성자
+    public QueryParameterNotFoundException() {
+        super();
+    }
+
+    // 메시지를 받는 생성자
+    public QueryParameterNotFoundException(String message) {
+        super(message);
+    }
+
+    // 메시지와 원인(cause)을 받는 생성자
+    public QueryParameterNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    // 원인(cause)을 받는 생성자
+    public QueryParameterNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
