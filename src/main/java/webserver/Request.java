d4c0316471201db99cff693929fb31e47efd1745
DrRivaski <leesm990708@gmail.com>
2024-07-05 11:16:06 +0900

    [mod] 쿼리스트링을 가지도록 수정 - 쿼리스트링 변수를 선언하여 쿼리스트링이 있을 경우 값을 가지도록 수정 - getStaticPath 메소드를 private으로 변경하고 getPath 메소드를 public으로 선언하여 외부에서는 getPath 메소드를 사용하도록 수정

--- src/main/java/webserver/Request.java
+++ src/main/java/webserver/Request.java
@@ -6,10 +6,18 @@ import java.util.HashMap;
 public class Request {
     private final String method;
     private final String path;
+    private final String queryString;
 
     protected Request(String requestLine) {
         this.method = requestLine.split(" ")[0];
-        this.path = requestLine.split(" ")[1];
+        String url = requestLine.split(" ")[1];
+        if (url.contains("?")) {
+            this.path = url.split("\\?")[0];
+            this.queryString = url.split("\\?")[1];
+        } else {
+            this.path = url;
+            this.queryString = "";
+        }
     }
 
     public static Request from(String line) {
@@ -19,10 +27,10 @@ public class Request {
     public boolean isQueryString() {
         // 일단은 "?"를 포함하는지만 검사.
         // 이후 예외처리 고민해볼 필요 있음
-        return path.contains("?");
+        return !queryString.isEmpty();
     }
 
-    public String getStaticPath() {
+    private String getStaticPath() {
         File testFile = new File("src/main/resources/static" + path);
 
         if (testFile.isDirectory()) {
@@ -37,22 +45,20 @@ public class Request {
     }
 
     public String getPath() {
-        return path;
+        return getStaticPath();
+    }
+
+    public String getQueryString() {
+        return this.queryString;
     }
 
     public HashMap<String, String> parseQueryString() {
-        String[] splitedQeuryString = splitQueryString(path);
+        String[] splitedQeuryString = queryString.split("&");
         HashMap<String, String> userData = new HashMap<>();
         for (String s : splitedQeuryString) {
             String[] fieldData = s.split("=");
             userData.put(fieldData[0], fieldData[1]);
         }
         return userData;
-    }
-
-    private String[] splitQueryString(String queryString) {
-        int questionMarkIndex = queryString.indexOf('?');
-        String dataString = queryString.substring(questionMarkIndex + 1);
-        return dataString.split("&");
     }
 }
