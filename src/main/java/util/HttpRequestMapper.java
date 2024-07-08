b582840539f6633bd75b0d10d287b4338ed130eb
win-luck <chaso1495@naver.com>
2024-07-08 17:25:42 +0900

    [refactor] 회원가입 post로 변경 및 HttpRequestMapper 개편

--- src/main/java/util/HttpRequestMapper.java
+++ src/main/java/util/HttpRequestMapper.java
@@ -4,9 +4,16 @@ package util;
  * 요청에 대한 매핑을 담당하는 enum 클래스
  */
 public enum HttpRequestMapper {
-    USER_SIGNUP("/user/create", "GET"),
+
+    // 200
+    STATIC("/index.html", "GET"),
+
+    // 302
+    USER_SIGNUP("/user/create", "POST"),
     REGISTER("/register.html", "GET"),
-    STATIC("/index.html", "GET");
+
+    // 400
+    MESSAGE_NOT_ALLOWED("/notallow", "GET");
 
     private final String path;
     private final String method;
@@ -18,8 +25,11 @@ public enum HttpRequestMapper {
 
     public static HttpRequestMapper of(String path, String method) {
         for (HttpRequestMapper mapper : values()) {
-            if (mapper.path.equals(path) && mapper.method.equals(method)) {
-                return mapper;
+            if(mapper.path.equals(path)){
+                if(mapper.method.equals(method)){
+                    return mapper;
+                }
+                return MESSAGE_NOT_ALLOWED; // 요청 경로는 찾았으나 메서드가 없는 경우
             }
         }
         return STATIC;
