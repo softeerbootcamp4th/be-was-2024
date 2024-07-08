76a97d8a0d755bc55af84a7446f748df84f7c24a
Jongseo Lee <jongseo12345@gmail.com>
2024-07-08 16:42:54 +0900

    fix: 회원가입 하는 방식 수정
    기존 GET 방식으로 진행하던 회원가입을 POST를 통한 회원가입으로 가능하게 코드 수정

--- src/main/java/util/DynamicRequestHandler.java
+++ src/main/java/util/DynamicRequestHandler.java
@@ -29,10 +29,11 @@ public class DynamicRequestHandler {
      */
     public void handle(HttpRequest request, HttpResponse response) {
         String path = request.getPath();
+        String method = request.getMethod();
         Map<String, String> queryParams = request.getQueryParams();
 
-        if (path.startsWith("/create")) {
-            handleCreateRequest(queryParams, response);
+        if (path.equals("/create") && method.equals("POST")) {
+            handleCreateUserRequest(request, response);
         }
 
     }
@@ -42,30 +43,50 @@ public class DynamicRequestHandler {
      * @param queryParams the query parameters
      * @param response the response to send
      */
-    private void handleCreateRequest(Map<String, String> queryParams, HttpResponse response) {
+    private void handleCreateUserRequest(HttpRequest request, HttpResponse response) {
         try {
-            String userId = URLDecoder.decode(queryParams.get("userId"), "UTF-8");
-            String password = URLDecoder.decode(queryParams.get("password"), "UTF-8");
-            String name = URLDecoder.decode(queryParams.get("name"), "UTF-8");
-            String email = URLDecoder.decode(queryParams.get("email"), "UTF-8");
+            // POST 데이터 파싱
+            String body = request.getBody();
+            Map<String, String> params = parseFormData(body);
 
+            String userId = params.get("userId");
+            String password = params.get("password");
+            String name = params.get("name");
+            String email = params.get("email");
+
+            // 사용자 생성
             Database.addUser(new User(userId, password, name, email));
 
             logUserCreation();
 
+            // 리다이렉트 응답
             Map<String, String> headers = new HashMap<>();
             headers.put("Location", "/index.html");
-            response.sendResponse(303, "See Other", headers, null);
-        } catch (UnsupportedEncodingException e) {
-            logger.debug("Error decoding query parameters: {}", e.getMessage());
+            response.sendResponse(302, "Found", headers, null);
+        } catch (Exception e) {
+            logger.error("Error handling create user request: {}", e.getMessage());
             sendErrorResponse(response, 400, "Bad Request");
         }
     }
+
+    private Map<String, String> parseFormData(String formData) throws UnsupportedEncodingException {
+        Map<String, String> params = new HashMap<>();
+        String[] pairs = formData.split("&");
+        for (String pair : pairs) {
+            int idx = pair.indexOf("=");
+            String key = URLDecoder.decode(pair.substring(0, idx), "UTF-8");
+            String value = URLDecoder.decode(pair.substring(idx + 1), "UTF-8");
+            params.put(key, value);
+        }
+        return params;
+    }
+
 
     /**
      * Logs the creation of a user.
      */
     private void logUserCreation() {
+        logger.debug("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!User created");
         for (User user : Database.findAll()) {
             logger.debug("User created: {}", user);
         }
