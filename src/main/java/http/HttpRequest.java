beda875c8a21afcd85b8360875eef36dd6ae6ab3
admin <admin@admins-MacBook-Pro.local>
2024-07-08 16:03:00 +0900

    HttpRequestParser 작성 : InputStream으로부터 HttpRequest 객체를 생성하는 부분은 HttpRequest 클래스 그 자체와는 관련성이 적다고 판단하여 요청을 파싱하여 생성하는 목적의 클래스를 분리함.

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -22,37 +22,6 @@ public class HttpRequest {
 
     public HttpRequest() {}
 
-    public static HttpRequest read(InputStream in) throws IOException {
-        HttpRequest request = new HttpRequest();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
-
-        String line = bufferedReader.readLine();
-        if(line == null || line.isEmpty()){
-            throw new IOException("Empty request line.");
-        }
-
-        String[] startLine = line.split(" ");
-        if(startLine.length != 3){
-            throw new IOException("Invalid HTTP request line:" + line);
-        }
-
-        request.setMethod(startLine[0].trim());
-        request.setUrl(startLine[1].trim());
-        request.setHttpVersion(startLine[2].trim());
-
-        while ((line = bufferedReader.readLine()) != null && !line.isEmpty()) { //null check only는 broken pipe 에러를 발생시킨다..?
-            int colonIndex = line.indexOf(':');
-            if (colonIndex == -1) {
-                throw new IOException("Invalid HTTP header: " + line);
-            }
-            String key = line.substring(0, colonIndex).trim();
-            String value = line.substring(colonIndex + 1).trim();
-            request.addHeader(key, value);
-        }
-
-        return request;
-    }
-
     public void setMethod(String method) {
         this.method = method;
     }
