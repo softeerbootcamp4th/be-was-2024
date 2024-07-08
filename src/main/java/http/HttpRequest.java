9e79362d12cdf91566d7ada981a0d1742f0210cd
k000927 <kimsungjin927@gmail.com>
2024-07-08 17:17:48 +0900

    feature: HttpRequest Getter 추가

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -1,4 +1,27 @@
 package http;
 
-public record HttpRequest(HttpMethod method, String url) {
+import java.util.HashMap;
+
+public class HttpRequest {
+    HttpMethod httpMethod;
+    String requestUrl;
+    HashMap<String, String> headers = new HashMap<>();
+
+    public HttpRequest(HttpMethod httpMethod, String requestUrl, HashMap<String, String> headers) {
+        this.httpMethod = httpMethod;
+        this.requestUrl = requestUrl;
+        this.headers = headers;
+    }
+
+    public HttpMethod getHttpMethod(){
+        return this.httpMethod;
+    }
+
+    public String getRequestUrl(){
+        return this.requestUrl;
+    }
+
+    public String getHeaders(String key){
+        return this.headers.get(key);
+    }
 }
