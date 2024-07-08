64dbb9c24cb85dd505b605c817af97206b41c0d5
k000927 <kimsungjin927@gmail.com>
2024-07-08 17:40:21 +0900

    test: 404 에러 발생하는지 확인하는 테스트 작성

--- src/test/java/test/UtilTest.java
+++ src/test/java/test/UtilTest.java
@@ -1,18 +1,32 @@
 package test;
 
+import http.HttpStatus;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import util.Utils;
 
+import java.io.IOException;
+
+import static util.Utils.*;
+import static util.Utils.getFileContent;
+
 public class UtilTest {
 
     @Test
     @DisplayName("staticPath가 올바르게 로딩되었는지 확인하는 테스트입니다.")
-    void isStaticPathLoaded(){
-        String staticPath = Utils.getStaticPath();
+    void isStaticPathLoaded() {
+        String staticPath = getStaticPath();
 
         Assertions.assertEquals(staticPath, "./src/main/resources/static");
     }
 
+    @Test
+    @DisplayName("올바르지 않은 경로가 들어왔을 때 404에러가 반환되는지 확인하는 테스트입니다.")
+    void is404returned() throws IOException {
+        String path = "/nothing";
+        ResponseWithStatus fileContent = getFileContent(path);
+
+        Assertions.assertEquals(fileContent.status, HttpStatus.NOT_FOUND);
+    }
 }
