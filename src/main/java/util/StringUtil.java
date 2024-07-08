2b42339a3d4da950e5dfaeb395bba6636b739595
win-luck <chaso1495@naver.com>
2024-07-08 17:29:15 +0900

    [chore] 상수 문자열 저장용 StringUtil 추가

---
+++ src/main/java/util/StringUtil.java
@@ -0,0 +1,17 @@
+package util;
+
+public class StringUtil {
+
+    private StringUtil() {
+    }
+
+    public static final String SPACE = " ";
+    public static final String SPACES = "\\s+";
+    public static final String COLON = ":";
+    public static final String COLON_WITH_SPACE = ": ";
+    public static final String CRLF = "\r\n";
+    public static final String AND = "&";
+    public static final String EQUAL = "=";
+    public static final String DOT = "\\.";
+    public static final String QUESTION_MARK = "\\?";
+}
