ef34f6e71fdb03d6aa34ce550c4c8ec94cbc4a7a
DrRivaski <leesm990708@gmail.com>
2024-07-08 16:28:09 +0900

    [mod] Request 생성 시 InputStream 전달하도록 수정

--- src/test/java/webserver/RequestTest.java
+++ src/test/java/webserver/RequestTest.java
@@ -3,17 +3,20 @@ package webserver;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
 import static org.assertj.core.api.Assertions.*;
 
 class RequestTest {
     @Test
     @DisplayName("GET과 query가 포함된 path가 들어왔을 때 isQueryString()이 정상적으로 동작하는지 테스트")
-    void isQueryStringTest() {
+    void isQueryStringTest() throws IOException {
         // given
         String qeury = "GET /user/create?userId=1234&name=qwer&password=asdf&email=wdwionv@wanef.com";
 
         // when
-        Request request = new Request(qeury);
+        Request request = Request.from(new ByteArrayInputStream(qeury.getBytes()));
 
         // then
         assertThat(request.isQueryString()).isEqualTo(true);
@@ -21,12 +24,12 @@ class RequestTest {
 
     @Test
     @DisplayName("GET과 qeury가 포함된 path가 들어왓을 때 path와 query string이 정상적으로 분리되는지 테스트")
-    void splitPathAndQeuryStringTest() {
+    void splitPathAndQeuryStringTest() throws IOException {
         // given
         String qeury = "GET /user/create?userId=1234&name=qwer&password=asdf&email=wdwionv@wanef.com";
 
         // when
-        Request request = new Request(qeury);
+        Request request = Request.from(new ByteArrayInputStream(qeury.getBytes()));
 
         // then
         assertThat(request.getPath()).isEqualTo("/user/create");
