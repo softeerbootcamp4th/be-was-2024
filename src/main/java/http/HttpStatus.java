4168f96587229a9a8df404f9b065966d1cefd902
k000927 <kimsungjin927@gmail.com>
2024-07-08 15:45:39 +0900

    refactor: HttpStatus Getter 작성

--- src/main/java/http/HttpStatus.java
+++ src/main/java/http/HttpStatus.java
@@ -4,9 +4,27 @@ public enum HttpStatus {
 
     OK(200, "C001", "success"),
     FOUND(302, "C002", "redirect"),
-    NOT_FOUND(404, "C003", "not found")
-    ;
+    NOT_FOUND(404, "C003", "not found");
+
+    private int status;
+    private String code;
+    private String message;
 
     HttpStatus(int status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getStatus(){
+        return status;
+    }
+
+    public String getCode(){
+        return code;
+    }
+
+    public String getMessage(){
+        return message;
     }
 }
