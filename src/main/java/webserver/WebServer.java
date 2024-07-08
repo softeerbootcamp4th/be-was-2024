bc6983974ed622f4c7a5f7400188766f1024043f
blaxsior <heejun2798@gmail.com>
2024-07-08 13:52:56 +0900

    refactor: 상수를 AppConfig로 이동

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 import chain.RouteHandleChain;
 import chain.StaticResourceChain;
 import chain.core.ChainManager;
+import config.AppConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import routehandler.route.NotFoundRouteHandler;
@@ -16,13 +17,11 @@ import routehandler.route.RegistrationRouteHandler;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
-    private static final int DEFAULT_PORT = 8080;
-    private static final int THREAD_POOL_SIZE = 10;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
         if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
+            port = AppConfig.DEFAULT_PORT;
         } else {
             port = Integer.parseInt(args[0]);
         }
@@ -36,7 +35,7 @@ public class WebServer {
                 )
         );
 
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(AppConfig.THREAD_POOL_SIZE);
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
