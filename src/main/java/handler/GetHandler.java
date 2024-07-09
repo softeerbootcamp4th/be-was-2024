cfa909d686d555cb5923d9927a1ea7010b822554
k000927 <kimsungjin927@gmail.com>
2024-07-09 15:57:40 +0900

    chore: 불필요한 코드들 삭제

--- src/main/java/handler/GetHandler.java
+++ src/main/java/handler/GetHandler.java
@@ -3,14 +3,11 @@ package handler;
 import http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import request.GetRequestHandler;
 import util.Utils.ResponseWithStatus;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Properties;
 
 import static util.ResponseFactory.*;
 import static util.ResponseFactory.addHeader;
@@ -18,20 +15,8 @@ import static util.Utils.*;
 
 public class GetHandler {
     private static final Logger log = LoggerFactory.getLogger(GetHandler.class);
-    private static final Properties properties = new Properties();
     private static String staticPath = getStaticPath();
 
-    static {
-        try (InputStream input = GetRequestHandler.class.getClassLoader().getResourceAsStream("config.properties")) {
-            if (input == null) {
-                log.error("Unable to find config.properties");
-            }
-            properties.load(input);
-            staticPath = properties.getProperty("staticPath");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void getRouter(String requestUrl, OutputStream out) throws IOException {
         String[] splitUrl = requestUrl.split("\\?");
