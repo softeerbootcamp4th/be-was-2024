604c04fae8d95d7de8c2abd707cc2f19904b430d
DrRivaski <leesm990708@gmail.com>
2024-07-09 15:08:24 +0900

    [mod] DisplayName 자세하게 수정

--- src/test/java/webserver/TypeTest.java
+++ src/test/java/webserver/TypeTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.*;
 class TypeTest {
 
     @Test
-    @DisplayName("resource handler get mime 테스트")
+    @DisplayName("resource handler의 get mime 테스트. ico를 입력하면 해당하는 image/x-icon 반환하는지 체크")
     void test() {
         // given
         ResourceHandler resourceHandler = new ResourceHandler();
