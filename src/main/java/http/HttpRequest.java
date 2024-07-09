ceaa37cd929673891f56ab325fc0327051d7f33b
k000927 <kimsungjin927@gmail.com>
2024-07-09 15:57:58 +0900

    feature: HttpRequest에 Setter 추가

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -1,27 +1,55 @@
 package http;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 public class HttpRequest {
+
     HttpMethod httpMethod;
     String requestUrl;
+    String version;
     HashMap<String, String> headers = new HashMap<>();
+    byte[] body;
 
-    public HttpRequest(HttpMethod httpMethod, String requestUrl, HashMap<String, String> headers) {
+
+    public void setStartLine(HttpMethod httpMethod, String requestUrl, String version) {
         this.httpMethod = httpMethod;
         this.requestUrl = requestUrl;
+        this.version = version;
+    }
+
+    public void setHeaders(HashMap<String, String> headers) {
         this.headers = headers;
     }
 
-    public HttpMethod getHttpMethod(){
+    public void setBody(byte[] body) {
+        this.body = body;
+    }
+
+    public HttpMethod getHttpMethod() {
         return this.httpMethod;
     }
 
-    public String getRequestUrl(){
+    public String getRequestUrl() {
         return this.requestUrl;
     }
 
-    public String getHeaders(String key){
-        return this.headers.get(key);
+    public Optional<String> getHeaders(String key) {
+        return Optional.ofNullable(headers.get(key));
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(httpMethod + " " + requestUrl + " " + version + "\n");
+        for (String s : headers.keySet()) {
+            sb.append(s + ": " + headers.get(s) + "\n");
+        }
+        if (body != null) {
+            sb.append("body : ");
+            for (byte b : body) {
+                sb.append((char) b);
+            }
+        }
+        return sb.toString();
     }
 }
