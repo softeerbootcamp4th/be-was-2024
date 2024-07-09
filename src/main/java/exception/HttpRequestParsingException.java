89c914597245c2cf0fdd5674fb195f7a56962b6d
hyeokson <chanhuk1765@gmail.com>
2024-07-09 17:27:56 +0900

    refactor: 다른 예외로 파싱 예외를 대체하기 위해 삭제

--- src/main/java/exception/HttpRequestParsingException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-// HttpRequest 파싱 예외
-public class HttpRequestParsingException extends RuntimeException {
-
-    public HttpRequestParsingException(String message) {
-        super(message);
-    }
-}
