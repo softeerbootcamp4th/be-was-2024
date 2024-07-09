b4334f87d2bab0e75c61f692f2172dc5867bd157
ysndy <ysndy@naver.com>
2024-07-09 14:34:38 +0900

    chore: nested 어노테이션 적용하여 테스트 수정

--- src/test/java/webserver/http/request/RequestTest.java
+++ src/test/java/webserver/http/request/RequestTest.java
@@ -1,6 +1,7 @@
 package webserver.http.request;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -9,8 +10,99 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RequestTest {
 
+    @Nested
+    @DisplayName("동등성 테스트")
+    class EqualityTest {
+
+        @Test
+        @DisplayName("Request 객체가 같다.")
+        void testEqualsTrue() {
+
+            //given
+            Request request1 = new Request.Builder(Method.GET, "/img/signiture.svg")
+                    .addHeader("Host", "localhost:8080")
+                    .addParameter("userId", "javajigi")
+                    .body(("body contents").getBytes())
+                    .build();
+
+            Request request2 = new Request.Builder(Method.GET, "/img/signiture.svg")
+                    .addHeader("Host", "localhost:8080")
+                    .addParameter("userId", "javajigi")
+                    .body(("body contents").getBytes())
+                    .build();
+
+            //when
+            boolean equality = request1.equals(request2);
+
+            //then
+            assertTrue(equality);
+
+        }
+
+        @Test
+        @DisplayName("Method 가 다르면 거짓")
+        void testEqualsFalseByMethod() {
+
+            //given
+            Request request1 = new Request.Builder(Method.GET, "/img/signiture.svg")
+                    .build();
+
+            Request request2 = new Request.Builder(Method.POST, "/img/signiture.svg")
+                    .build();
+
+            //when
+            boolean equality = request1.equals(request2);
+
+            //then
+            assertFalse(equality);
+
+        }
+
+        @Test
+        @DisplayName("Path 가 다르면 거짓")
+        void testEqualsFalseByPath() {
+
+            //given
+            Request request1 = new Request.Builder(Method.GET, "/img/signiture.jpg")
+                    .build();
+
+            Request request2 = new Request.Builder(Method.GET, "/img/signiture.svg")
+                    .build();
+
+            //when
+            boolean equality = request1.equals(request2);
+
+            //then
+            assertFalse(equality);
+
+        }
+
+        @Test
+        @DisplayName("Header 가 다르면 거짓")
+        void testEqualsFalseByHeader() {
+
+            //given
+            Request request1 = new Request.Builder(Method.GET, "/img/signiture.svg")
+                    .addHeader("Host", "localhost:7080")
+                    .build();
+
+            Request request2 = new Request.Builder(Method.GET, "/img/signiture.svg")
+                    .addHeader("Host", "localhost:8080")
+                    .build();
+
+            //when
+            boolean equality = request1.equals(request2);
+
+            //then
+            assertFalse(equality);
+
+        }
+
+
+    }
+
     @Test
-    @DisplayName("Body가 없는 요청에 대해 정상적으로 파싱 가능한지 테스트")
+    @DisplayName("Body가 없는 요청에 대해 정상적으로 파싱 가능")
     void testParseRequestWithoutBody() throws IOException {
 
         //given
@@ -34,91 +126,7 @@ class RequestTest {
     }
 
     @Test
-    @DisplayName("Request 객체의 값 동등성에 대해 참인지 테스트")
-    void testEqualsTrue() {
-
-        //given
-        Request request1 = new Request.Builder(Method.GET, "/img/signiture.svg")
-                .addHeader("Host", "localhost:8080")
-                .addParameter("userId", "javajigi")
-                .body(("body contents").getBytes())
-                .build();
-
-        Request request2 = new Request.Builder(Method.GET, "/img/signiture.svg")
-                .addHeader("Host", "localhost:8080")
-                .addParameter("userId", "javajigi")
-                .body(("body contents").getBytes())
-                .build();
-
-        //when
-        boolean equality = request1.equals(request2);
-
-        //then
-        assertTrue(equality);
-
-    }
-
-    @Test
-    @DisplayName("Request 객체의 값 동등성에 대해 메소드가 다르면 거짓인지 테스트")
-    void testEqualsFalseByMethod() {
-
-        //given
-        Request request1 = new Request.Builder(Method.GET, "/img/signiture.svg")
-                .build();
-
-        Request request2 = new Request.Builder(Method.POST, "/img/signiture.svg")
-                .build();
-
-        //when
-        boolean equality = request1.equals(request2);
-
-        //then
-        assertFalse(equality);
-
-    }
-
-    @Test
-    @DisplayName("Request 객체의 값 동등성에 대해 패스가 다르면 거짓인지 테스트")
-    void testEqualsFalseByPath() {
-
-        //given
-        Request request1 = new Request.Builder(Method.GET, "/img/signiture.jpg")
-                .build();
-
-        Request request2 = new Request.Builder(Method.GET, "/img/signiture.svg")
-                .build();
-
-        //when
-        boolean equality = request1.equals(request2);
-
-        //then
-        assertFalse(equality);
-
-    }
-
-    @Test
-    @DisplayName("Request 객체의 값 동등성에 대해 헤더가 다르면 거짓인지 테스트")
-    void testEqualsFalseByHeader() {
-
-        //given
-        Request request1 = new Request.Builder(Method.GET, "/img/signiture.svg")
-                .addHeader("Host", "localhost:7080")
-                .build();
-
-        Request request2 = new Request.Builder(Method.GET, "/img/signiture.svg")
-                .addHeader("Host", "localhost:8080")
-                .build();
-
-        //when
-        boolean equality = request1.equals(request2);
-
-        //then
-        assertFalse(equality);
-
-    }
-
-    @Test
-    @DisplayName("Request 객체를 String으로 변환")
+    @DisplayName("String 변환 테스트")
     void testToString(){
 
         //given
--- src/test/java/webserver/http/response/ResponseHandlerTest.java
+++ src/test/java/webserver/http/response/ResponseHandlerTest.java
@@ -3,6 +3,7 @@ package webserver.http.response;
 import db.Database;
 import model.User;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import webserver.http.request.Method;
 import webserver.http.request.Request;
@@ -14,115 +15,122 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ResponseHandlerTest {
 
-    @Test
-    @DisplayName("index.html 요청에 대한 응답 성공")
-    void testResponseIndexHtmlSuccess() throws IOException {
-        //given
-        Request request = new Request.Builder(Method.GET, "/index.html")
-                .addHeader("Host", "localhost:8080")
-                .build();
+    @Nested
+    @DisplayName("정적 요청에 대한 응답")
+    class StaticResponseTest {
 
-        Response expected = new Response.Builder(Status.OK)
-                .addHeader("Content-Type", "text/html;charset=utf-8")
-                .body(getFile("index.html"))
-                .build();
+        @Test
+        @DisplayName("HTML")
+        void testResponseIndexHtmlSuccess() throws IOException {
+            //given
+            Request request = new Request.Builder(Method.GET, "/index.html")
+                    .addHeader("Host", "localhost:8080")
+                    .build();
 
-        //when
-        Response actual = ResponseHandler.response(request);
+            Response expected = new Response.Builder(Status.OK)
+                    .addHeader("Content-Type", "text/html;charset=utf-8")
+                    .body(getFile("index.html"))
+                    .build();
 
-        //then
-        assertEquals(expected,actual);
+            //when
+            Response actual = ResponseHandler.response(request);
 
-    }
+            //then
+            assertEquals(expected,actual);
 
-    @Test
-    @DisplayName("png 요청에 대한 응답 성공")
-    void testAccessStaticResourcePngSuccess() throws IOException {
-        //given
-        Request request = new Request.Builder(Method.GET, "/img/signiture.png")
-                .addHeader("Host", "localhost:8080")
-                .addHeader("Connection", "keep-alive")
-                .build();
+        }
 
-        Response expected = new Response.Builder(Status.OK)
-                .addHeader("Content-Type", "image/png;charset=utf-8")
-                .body(getFile("/img/signiture.png"))
-                .build();
+        @Test
+        @DisplayName("PNG")
+        void testAccessStaticResourcePngSuccess() throws IOException {
+            //given
+            Request request = new Request.Builder(Method.GET, "/img/signiture.png")
+                    .addHeader("Host", "localhost:8080")
+                    .addHeader("Connection", "keep-alive")
+                    .build();
 
-        //when
-        Response actual = ResponseHandler.response(request);
+            Response expected = new Response.Builder(Status.OK)
+                    .addHeader("Content-Type", "image/png;charset=utf-8")
+                    .body(getFile("/img/signiture.png"))
+                    .build();
 
-        //then
-        assertEquals(expected,actual);
+            //when
+            Response actual = ResponseHandler.response(request);
 
-    }
+            //then
+            assertEquals(expected,actual);
 
-    @Test
-    @DisplayName("svg 요청에 대한 응답 성공")
-    void testAccessStaticResourceSvgSuccess() throws IOException {
+        }
 
-        //given
-        Request request = new Request.Builder(Method.GET, "/img/signiture.svg")
-                .addHeader("Host", "localhost:8080")
-                .addHeader("Connection", "keep-alive")
-                .build();
+        @Test
+        @DisplayName("SVG")
+        void testAccessStaticResourceSvgSuccess() throws IOException {
 
-        Response expected = new Response.Builder(Status.OK)
-                .addHeader("Content-Type", "image/svg+xml;charset=utf-8")
-                .body(getFile("/img/signiture.svg"))
-                .build();
+            //given
+            Request request = new Request.Builder(Method.GET, "/img/signiture.svg")
+                    .addHeader("Host", "localhost:8080")
+                    .addHeader("Connection", "keep-alive")
+                    .build();
 
-        //when
-        Response actual = ResponseHandler.response(request);
+            Response expected = new Response.Builder(Status.OK)
+                    .addHeader("Content-Type", "image/svg+xml;charset=utf-8")
+                    .body(getFile("/img/signiture.svg"))
+                    .build();
 
-        //then
-        assertEquals(expected,actual);
-    }
+            //when
+            Response actual = ResponseHandler.response(request);
+
+            //then
+            assertEquals(expected,actual);
+        }
 
 
-    @Test
-    @DisplayName("css 요청에 대한 응답 성공")
-    void testAccessStaticResourceCssSuccess() throws IOException {
+        @Test
+        @DisplayName("CSS")
+        void testAccessStaticResourceCssSuccess() throws IOException {
 
-        //given
-        Request request = new Request.Builder(Method.GET, "/main.css")
-                .addHeader("Host", "localhost:8080")
-                .addHeader("Connection", "keep-alive")
-                .build();
+            //given
+            Request request = new Request.Builder(Method.GET, "/main.css")
+                    .addHeader("Host", "localhost:8080")
+                    .addHeader("Connection", "keep-alive")
+                    .build();
 
-        Response expected = new Response.Builder(Status.OK)
-                .addHeader("Content-Type", "text/css;charset=utf-8")
-                .body(getFile("main.css"))
-                .build();
+            Response expected = new Response.Builder(Status.OK)
+                    .addHeader("Content-Type", "text/css;charset=utf-8")
+                    .body(getFile("main.css"))
+                    .build();
 
-        //when
-        Response actual = ResponseHandler.response(request);
+            //when
+            Response actual = ResponseHandler.response(request);
 
-        //then
-        assertEquals(expected,actual);
+            //then
+            assertEquals(expected,actual);
 
-    }
+        }
 
-    @Test
-    @DisplayName("ico 요청에 대한 응답 성공")
-    void testAccessStaticResourceIcoSuccess() throws IOException {
+        @Test
+        @DisplayName("ICO")
+        void testAccessStaticResourceIcoSuccess() throws IOException {
 
-        //given
-        Request request = new Request.Builder(Method.GET, "/favicon.ico")
-                .addHeader("Host", "localhost:8080")
-                .addHeader("Connection", "keep-alive")
-                .build();
+            //given
+            Request request = new Request.Builder(Method.GET, "/favicon.ico")
+                    .addHeader("Host", "localhost:8080")
+                    .addHeader("Connection", "keep-alive")
+                    .build();
 
-        Response expected = new Response.Builder(Status.OK)
-                .addHeader("Content-Type", "image/png;charset=utf-8")
-                .body(getFile("favicon.ico"))
-                .build();
+            Response expected = new Response.Builder(Status.OK)
+                    .addHeader("Content-Type", "image/png;charset=utf-8")
+                    .body(getFile("favicon.ico"))
+                    .build();
 
-        //when
-        Response actual = ResponseHandler.response(request);
+            //when
+            Response actual = ResponseHandler.response(request);
 
-        //then
-        assertEquals(expected,actual);
+            //then
+            assertEquals(expected,actual);
+
+        }
+
 
     }
 
