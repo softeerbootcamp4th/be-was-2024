9a8989e1a2e81c2cfea00b5297e79680a1c042a4
hyeokson <chanhuk1765@gmail.com>
2024-07-09 23:41:04 +0900

    feat: RequestHandler의 작업을 수행하는 Dispatcher 클래스를 생성하여 기능 분리

---
+++ src/main/java/webserver/Dispatcher.java
@@ -0,0 +1,60 @@
+package webserver;
+
+import com.sun.net.httpserver.Request;
+import constant.HttpStatus;
+import dto.HttpRequest;
+import dto.HttpResponse;
+import exception.InvalidHttpRequestException;
+import handler.Handler;
+import handler.HandlerManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.HttpRequestParser;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Dispatcher {
+    private static final Logger logger = LoggerFactory.getLogger(Dispatcher.class);
+    private final HandlerManager handlerManager = HandlerManager.getInstance();
+
+    private Dispatcher() {}
+
+    private static class LazyHolder {
+        private static final Dispatcher INSTANCE = new Dispatcher();
+    }
+
+    public static Dispatcher getInstance() {
+        return Dispatcher.LazyHolder.INSTANCE;
+    }
+
+    public void dispatch(BufferedReader br, DataOutputStream dos) throws IOException {
+        try {
+            // HttpRequest 파싱 및 결과 반환
+            HttpRequest httpRequest = HttpRequestParser.parseHttpRequest(br);
+
+            // HandlerManager를 통해 request를 처리할 수 있는 handler 반환
+            Handler handler = handlerManager.getHandler(httpRequest);
+
+            // handler의 결과를 저장할 HttpResponse 객체 생성
+            HttpResponse httpResponse = new HttpResponse();
+
+            // handler 실행
+            handler.handle(httpRequest, httpResponse);
+
+            // HttpResponse를 client에게 응답
+            httpResponse.sendHttpResponse(dos);
+        }
+        catch (IOException | IllegalArgumentException | InvalidHttpRequestException e){
+            logger.error(e.getMessage());
+
+            // 에러 응답을 저장할 HttpResponse 객체 생성
+            HttpResponse httpResponse = new HttpResponse();
+            httpResponse.setHttpStatus(HttpStatus.NOT_FOUND);
+            httpResponse.sendHttpResponse(dos);
+        }
+    }
+}
