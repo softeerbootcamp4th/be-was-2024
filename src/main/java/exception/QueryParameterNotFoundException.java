b667c44fd7936f3d1a757679d0a117871f3ed916
admin <admin@admins-MacBook-Pro.local>
2024-07-09 10:30:42 +0900

    UnsupportedHttpVersionException 생성 및 RequestHandler 지원 코드 작성. : 서버가 지원하지 않는 HttpVersion으로 들어온 정당한 요청은 InvalidHttpRequestException이 아닌 Unsupported- 예외를 발생시키도록 구분함. 이에 따라 RequestHandler에서 Invalid, Unsupported 요청을 처리하는 catch 문 작성. Invalid한 경우 400, Unsupported인 경우 505 코드를 발생시키도록 하였다.

--- src/main/java/exception/QueryParameterNotFoundException.java
+++ src/main/java/exception/QueryParameterNotFoundException.java
@@ -1,22 +1,18 @@
 package exception;
 
 public class QueryParameterNotFoundException extends RuntimeException {
-    // 기본 생성자
     public QueryParameterNotFoundException() {
         super();
     }
 
-    // 메시지를 받는 생성자
     public QueryParameterNotFoundException(String message) {
         super(message);
     }
 
-    // 메시지와 원인(cause)을 받는 생성자
     public QueryParameterNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    // 원인(cause)을 받는 생성자
     public QueryParameterNotFoundException(Throwable cause) {
         super(cause);
     }
