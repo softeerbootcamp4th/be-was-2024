ccb5258d6bad58848406ff09776634eacd5fc505
blaxsior <heejun2798@gmail.com>
2024-07-09 13:59:11 +0900

    fix: RouteHandleChain에 예외처리 추가, 이후 체인으로 넘어갈 수 있게 함

--- src/main/java/chain/RouteHandleChain.java
+++ src/main/java/chain/RouteHandleChain.java
@@ -4,16 +4,15 @@ import chain.core.MiddlewareChain;
 import http.MyHttpRequest;
 import http.MyHttpResponse;
 import http.enums.HttpMethodType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import routehandler.core.IRouteHandler;
 import routehandler.core.trie.RouteTrie;
 import routehandler.utils.Route;
 import routehandler.utils.RouteRecord;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class RouteHandleChain extends MiddlewareChain {
+    private static Logger logger = LoggerFactory.getLogger(RouteHandleChain.class);
     private final RouteTrie trie;
 
     public RouteHandleChain() {
@@ -42,8 +41,13 @@ public class RouteHandleChain extends MiddlewareChain {
         String pathname = req.getUrl().getPathname();
         HttpMethodType method = req.getMethod();
 
-        IRouteHandler handler = trie.search(pathname,method);
-        handler.handle(req, res);
+        try {
+            IRouteHandler handler = trie.search(pathname,method);
+            handler.handle(req, res);
+        } catch (Exception e) {
+            // 핸들러에 의해 처리될 수 없는 상황 다음으로 넘긴다.
+            logger.error(e.getMessage());
+        }
 
         next(req, res);
     }
