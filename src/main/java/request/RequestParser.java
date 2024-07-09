cad9e6e333b6e94d4150acfa9c627ceb1685a327
k000927 <kimsungjin927@gmail.com>
2024-07-09 15:59:49 +0900

    refactor: Request에서 StartLine, Header, Body 파싱 메서드 분할 - contentLength 헤더 존재 시 body 파싱 진행 - 헤더 파싱 시 정규표현식 사용하도록 리팩토링 진행 예정

--- src/main/java/request/RequestParser.java
+++ src/main/java/request/RequestParser.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.HashMap;
+import java.util.Optional;
 
 import static handler.Router.requestMapping;
 
@@ -16,32 +17,64 @@ public class RequestParser {
     public void ParsingRequest(InputStream in, OutputStream out) throws IOException {
         InputStreamReader inputStreamReader = new InputStreamReader(in);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        StringBuilder log = new StringBuilder().append("\n\n****** REQUEST ******\n");
 
-        String firstLine = bufferedReader.readLine();
-        String[] request = firstLine.split(" ");
+        HttpRequest request = new HttpRequest();
 
-        HttpMethod method = HttpMethod.getMethod(request[0]);
 
-        String requestUrl = request[1];
-        HashMap<String, String> header = new HashMap<>();
+        setStartLine(bufferedReader, request, log);
+        setHeaders(bufferedReader, request, log);
+        setBody(bufferedReader, request, log);
 
-        StringBuilder log = new StringBuilder().append("\n\n***** REQUEST *****\n").append(firstLine + "\n");
+        logger.debug(log.toString());
 
-        String headerLine = bufferedReader.readLine();
-        while (headerLine != null && !headerLine.isEmpty()) {
-            log.append(headerLine + "\n");
+        requestMapping(request, out);
+    }
+
+    private void setStartLine(BufferedReader bufferedReader, HttpRequest request, StringBuilder log) throws IOException {
+        String startLine = bufferedReader.readLine();
+        log.append(startLine).append("\n");
+
+        String[] splitStartLine = startLine.split(" ");
+
+        HttpMethod method = HttpMethod.getMethod(splitStartLine[0]);
+        String requestUrl = splitStartLine[1];
+        String version = splitStartLine[2];
+
+        request.setStartLine(method, requestUrl, version);
+    }
+
+    private void setHeaders(BufferedReader bufferedReader, HttpRequest request, StringBuilder log) throws IOException {
+        HashMap<String, String> headers = new HashMap<>();
+        String headerLine = "";
+
+        while (!(headerLine = bufferedReader.readLine()).isEmpty()) {
+            log.append(headerLine).append("\n");
 
             String[] headerParts = headerLine.split(":");
 
             String key = headerParts[0].strip();
             String value = headerParts[1].strip();
-            header.put(key, value);
 
-            headerLine = bufferedReader.readLine();
+            // 문제 발생 가능 -> 정규표현식 이용해서 다시 작성하기
+
+            headers.put(key, value);
         }
-        logger.debug(log.toString());
 
-        HttpRequest httpRequest = new HttpRequest(method, requestUrl, header);
-        requestMapping(httpRequest, out);
+        log.append("\n");
+        request.setHeaders(headers);
+    }
+
+    private void setBody(BufferedReader bufferedReader, HttpRequest request, StringBuilder log) throws IOException {
+        Optional<String> optionalContentLength = request.getHeaders("Content-Length");
+        if (optionalContentLength.isEmpty()) return;
+
+        int contentLength = Integer.parseInt(optionalContentLength.get());
+        byte[] body = new byte[contentLength];
+
+        for (int i = 0; i < contentLength; i++) {
+            body[i] = (byte) bufferedReader.read();
+        }
+        request.setBody(body);
     }
 }
