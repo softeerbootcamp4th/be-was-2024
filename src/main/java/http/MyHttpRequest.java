d566b0bc1ed7feba460a39bf7f5c4f0a9405566c
blaxsior <heejun2798@gmail.com>
2024-07-09 14:04:28 +0900

    feat: 회원가입 핸들러 추가, http request 생성자 종류 추가

--- src/main/java/http/MyHttpRequest.java
+++ src/main/java/http/MyHttpRequest.java
@@ -13,8 +13,17 @@ public class MyHttpRequest {
     private final String version;
 
     private final MyHttpHeaders headers;
+    private byte[] body;
 
-    public MyHttpRequest(String requestLine, List<String> headerLines) {
+    public MyHttpRequest(HttpMethodType method, MyURL url, String version, MyHttpHeaders headers, byte[] body) {
+        this.method = method;
+        this.url = url;
+        this.version = version;
+        this.headers = headers;
+        this.body = body;
+    }
+
+    public MyHttpRequest(String requestLine, List<String> headerLines, byte[] body) {
         String[] reqLineTokens = HttpParseUtil.parseRequestLine(requestLine);
         this.method = HttpMethodTypeUtil.getHttpMethodType(reqLineTokens[0]);
         this.url = new MyURL(reqLineTokens[1]);
@@ -22,7 +31,21 @@ public class MyHttpRequest {
 
         headers = new MyHttpHeaders();
         headers.putHeaders(headerLines);
+        // body는 초기화하지 않은 상태로 취급. 나중에 설정해야 함.
+        this.body = body;
     }
+
+    public MyHttpRequest(String requestLine, MyHttpHeaders headers, byte[] body) {
+        String[] reqLineTokens = HttpParseUtil.parseRequestLine(requestLine);
+        this.method = HttpMethodTypeUtil.getHttpMethodType(reqLineTokens[0]);
+        this.url = new MyURL(reqLineTokens[1]);
+        this.version = reqLineTokens[2];
+
+        this.headers = headers;
+        this.body = body;
+    }
+
+
 
     public MyURL getUrl() {
         return url;
@@ -38,5 +61,13 @@ public class MyHttpRequest {
 
     public MyHttpHeaders getHeaders() {
         return headers;
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+
+    public void setBody(byte[] body) {
+        this.body = body;
     }
 }
