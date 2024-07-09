72c7e30cf90ba3ab878fb1b9156bdde08fa07e61
blaxsior <heejun2798@gmail.com>
2024-07-09 14:03:55 +0900

    feat, refactor: request 생성 로직을 별도 메서드로 분리, body도 읽게 변경
    TODO: byte 기반으로 데이터 처리하도록 수정할 필요 있음. 현재는 bufferedReader을 이용해서 문자열로 읽는 중. byte 데이터를 그대로 다룰 수 있게 조정 필요

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,15 +2,19 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 import chain.core.ChainManager;
+import http.MyHttpHeaders;
 import http.MyHttpRequest;
 import http.MyHttpResponse;
+import http.enums.HttpMethodType;
+import http.utils.HttpMethodTypeUtil;
+import http.utils.HttpParseUtil;
 import http.utils.HttpResponseMessageBuildUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import routehandler.core.IRouteHandler;
 import url.MyURL;
 
 public class RequestHandler implements Runnable {
@@ -19,8 +23,7 @@ public class RequestHandler implements Runnable {
     private final Socket connection;
     private final ChainManager chainManager;
 
-    public RequestHandler(Socket connectionSocket, ChainManager chainManager )
-    {
+    public RequestHandler(Socket connectionSocket, ChainManager chainManager) {
         this.connection = connectionSocket;
         this.chainManager = chainManager;
     }
@@ -30,22 +33,8 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 
-            /** request parsing phase **/
-            // 맨 처음 요청 라인 읽기
-            String requestLine = br.readLine();
-            // header 목록 읽기
-            List<String> headerLines = new ArrayList<>();
-            String headerLine;
-            while (!(headerLine = br.readLine()).isEmpty()) {
-                headerLines.add(headerLine);
-            }
-            // body 부분은 나중에 추가적으로 확장
-
-            /** handle & response phase **/
-
-            MyHttpRequest request = new MyHttpRequest(requestLine, headerLines);
+            MyHttpRequest request = buildRequest(in);
             MyHttpResponse response = new MyHttpResponse(request.getVersion());
             logger.debug("request url: {}", request.getUrl().getPathname());
 
@@ -54,12 +43,46 @@ public class RequestHandler implements Runnable {
             DataOutputStream dos = new DataOutputStream(out);
 
             byte[] responseMessage = HttpResponseMessageBuildUtil.build(response);
-            logger.debug(new String(responseMessage));
             sendResponse(dos, responseMessage);
             logger.debug("send response");
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private MyHttpRequest buildRequest(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+
+        /** request parsing phase **/
+        // 맨 처음 요청 라인 읽기
+        String requestLine = br.readLine();
+        String[] reqLineItems = HttpParseUtil.parseRequestLine(requestLine);
+        // 요청 라인 데이터 추출
+        HttpMethodType method = HttpMethodTypeUtil.getHttpMethodType(reqLineItems[0]);
+        MyURL url = new MyURL(reqLineItems[1]);
+        String version = reqLineItems[2];
+
+        // header 목록 읽기
+        List<String> headerLines = new ArrayList<>();
+        String headerLine;
+        while (!(headerLine = br.readLine()).isEmpty()) {
+            headerLines.add(headerLine);
+        }
+
+        // 헤더 객체 생성
+        MyHttpHeaders headers = new MyHttpHeaders();
+        headers.putHeaders(headerLines);
+
+        // body 읽기
+        char[] charbody = new char[headers.getContentLength()];
+        byte[] body = new byte[0];
+
+        if(charbody.length > 0) {
+            br.read(charbody);
+            body = new String(charbody).getBytes(StandardCharsets.UTF_8);
+        }
+
+        return new MyHttpRequest(method, url, version, headers, body);
     }
 
     private void sendResponse(DataOutputStream dos, byte[] responseMessage) {
