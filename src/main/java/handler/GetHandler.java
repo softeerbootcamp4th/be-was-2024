ed1da23301e04038df0c0e871bf03741be58c8d2
k000927 <kimsungjin927@gmail.com>
2024-07-09 16:12:19 +0900

    refactor: 라우팅 관련 책임 Router로 위임

--- src/main/java/handler/GetHandler.java
+++ src/main/java/handler/GetHandler.java
@@ -1,5 +1,6 @@
 package handler;
 
+import http.HttpRequest;
 import http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,24 +18,13 @@ public class GetHandler {
     private static final Logger log = LoggerFactory.getLogger(GetHandler.class);
     private static String staticPath = getStaticPath();
 
-
-    public static void getRouter(String requestUrl, OutputStream out) throws IOException {
-        String[] splitUrl = requestUrl.split("\\?");
-        String requestURL = splitUrl[0];
-
-        switch (requestURL) {
-            case "/", "/registration", "/login" -> sendResponse(staticPath + requestURL + "/index.html", out);
-            default -> sendResponse(staticPath + requestURL, out);
-        }
-    }
-
-    private static void sendResponse(String requestUrl, OutputStream out) throws IOException {
+    public static void sendResponse(String requestUrl, OutputStream out) throws IOException {
         DataOutputStream dos = new DataOutputStream(out);
 
         String[] tokens = requestUrl.split("\\.");
         String type = tokens[tokens.length - 1];
 
-        ResponseWithStatus responseWithStatus = getFileContent(requestUrl);
+        ResponseWithStatus responseWithStatus = getFileContent(staticPath + requestUrl);
 
         HttpStatus status = responseWithStatus.status;
         byte[] body = responseWithStatus.body;
--- src/main/java/handler/Router.java
+++ src/main/java/handler/Router.java
@@ -7,14 +7,28 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 import static handler.GetHandler.getRouter;
+import static handler.GetHandler.sendResponse;
+import static handler.PostHandler.postRouter;
 
 public class Router {
     public static void requestMapping(HttpRequest httpRequest, OutputStream out) throws IOException {
         HttpMethod method = httpRequest.getHttpMethod();
-        String requestUrl = httpRequest.getRequestUrl();
 
         switch (method){
-            case GET -> getRouter(requestUrl, out);
+            case GET -> getRequestMapping(httpRequest, out);
+            case POST -> postRouter(httpRequest, out);
+        }
+    }
+
+    private static void getRequestMapping(HttpRequest httpRequest, OutputStream out) throws IOException {
+        String requestUrl = httpRequest.getRequestUrl();
+
+        String[] splitUrl = requestUrl.split("\\?");
+        String requestTarget = splitUrl[0];
+
+        switch (requestTarget) {
+            case "/", "/registration", "/login" -> sendResponse(requestTarget + "/index.html", out);
+            default -> sendResponse(requestTarget, out);
         }
     }
 }
