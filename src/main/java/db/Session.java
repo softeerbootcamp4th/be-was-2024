c2297011adef40395fdaebf5a81f08c8623268e0
repitor222 <repitor15@gmail.com>
2024-07-10 13:51:00 +0900

    [feat] Session 클래스 session 추가 및 조회 메서드 추가
    addSessionId 메서드를 통한 유저 별 세션 추가 가능
    findUserBySessionId 메서드를 통한 세션으로 유저 조회 가능

--- src/main/java/db/Session.java
+++ src/main/java/db/Session.java
@@ -7,4 +7,10 @@ import java.util.Map;
 
 public class Session {
     private static Map<String, User> users = new HashMap<>();
+
+    public static void addSessionId(String sessionId, User user) {users.put(sessionId, user);}
+
+    public static User findUserBySessionId(String sessionId) {
+        return users.get(sessionId);
+    }
 }
