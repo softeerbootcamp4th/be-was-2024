87d12dfd217673f09fdf393c27c7f7e0ece36309
DrRivaski <leesm990708@gmail.com>
2024-07-10 15:58:41 +0900

    [mod] 빌더 패턴을 이용하여 생성하도록 수정 - url, redirect code, dos를 받아올 수 있도록 빌더 클래스 구현

--- src/main/java/webserver/Response.java
+++ src/main/java/webserver/Response.java
@@ -8,6 +8,51 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class Response {
+    private final String url;
+    private final int redirectCode;
+    private final int statusCode;
+    private final DataOutputStream dos;
+
+
+    private Response(Builder builder) {
+        this.url = builder.url;
+        this.redirectCode = builder.redirectCode;
+        this.statusCode = builder.statusCode;
+        this.dos = builder.dos;
+    }
+
+    public static class Builder {
+        private String url;
+        private int redirectCode = 0;
+        private int statusCode = 0;
+        private DataOutputStream dos;
+
+        public Builder url(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public Builder redirectCode(int redirectCode) {
+            this.redirectCode = redirectCode;
+            return this;
+        }
+
+        public Builder statusCode(int statusCode) {
+            this.statusCode = statusCode;
+            return this;
+        }
+
+        public Builder dataOutputStream(DataOutputStream dos) {
+            this.dos = dos;
+            return this;
+        }
+
+        public Response build() {
+            return new Response(this);
+        }
+    }
+
+
     private static final Logger logger = LoggerFactory.getLogger(Response.class);
 
     private static final ResourceHandler resourceHandler = new ResourceHandler();
@@ -20,6 +65,14 @@ public class Response {
 
     }
 
+    public void sendResponse() throws IOException {
+        if (redirectCode != 0) {
+            redirect(url, dos, redirectCode);
+        } else {
+            response(url, dos);
+        }
+    }
+
     public void response(String url, DataOutputStream dos) throws IOException {
         // url로부터 html파일을 byte array로 읽어오기
         byte[] body = resourceHandler.getByteArray(url);
@@ -28,7 +81,7 @@ public class Response {
         responseBody(dos, body);
     }
 
-    public void redirect(String url, DataOutputStream dos, int redirectionCode)  throws IOException {
+    public void redirect(String url, DataOutputStream dos, int redirectionCode) throws IOException {
         if (redirectionCode == 301) {
             response301Header(dos, url);
         } else if (redirectionCode == 302) {
@@ -39,6 +92,7 @@ public class Response {
             responseBody(dos, body);
         }
     }
+
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
         try {
@@ -92,4 +146,6 @@ public class Response {
             logger.error(e.getMessage());
         }
     }
+
+
 }
