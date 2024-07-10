c887bf9afce54ccfb2d9b049b3d8bafea1b0cdb2
repitor222 <repitor15@gmail.com>
2024-07-10 13:41:13 +0900

    [fix] WebServer thread 조정
    cachedThreadPool -> SingleThreadExecutor로 조정

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -23,7 +23,7 @@ public class WebServer {
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
-            ExecutorService executor = Executors.newCachedThreadPool();
+            ExecutorService executor = Executors.newSingleThreadExecutor();
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
