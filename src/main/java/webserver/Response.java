d45c72330072b4659697338fd2d0ce3e662370d1
DrRivaski <leesm990708@gmail.com>
2024-07-10 18:34:08 +0900

    [mod] 쿠키를 관리하기 위한 변수 cookie 선언 - 빌더 클래스에 cookie 생성 메소드 추가 - 쿠키를 포함한 response 메소드 추가

--- src/main/java/webserver/Response.java
+++ src/main/java/webserver/Response.java
@@ -12,13 +12,14 @@ public class Response {
     private final int redirectCode;
     private final int statusCode;
     private final DataOutputStream dos;
-
+    private final String cookie;
 
     private Response(Builder builder) {
         this.url = builder.url;
         this.redirectCode = builder.redirectCode;
         this.statusCode = builder.statusCode;
         this.dos = builder.dos;
+        this.cookie = builder.cookie;
     }
 
     public static class Builder {
@@ -26,6 +27,7 @@ public class Response {
         private int redirectCode = 0;
         private int statusCode = 0;
         private DataOutputStream dos;
+        private String cookie = "";
 
         public Builder url(String url) {
             this.url = url;
@@ -47,11 +49,15 @@ public class Response {
             return this;
         }
 
+        public Builder cookie(String sessionId) {
+            this.cookie = sessionId;
+            return this;
+        }
+
         public Response build() {
             return new Response(this);
         }
     }
-
 
     private static final Logger logger = LoggerFactory.getLogger(Response.class);
 
@@ -69,11 +75,36 @@ public class Response {
         // url로부터 html파일을 byte array로 읽어오기
         byte[] body = resourceHandler.getByteArray(url);
 
-        response200Header(dos, body.length, resourceHandler.getContentType(url));
+        if (cookie.isEmpty()) {
+            response200Header(dos, body.length, resourceHandler.getContentType(url));
+        } else {
+            response200HeaderWithCookie(dos, body.length, resourceHandler.getContentType(url));
+        }
+
         responseBody(dos, body);
     }
 
     public void redirect(String url, DataOutputStream dos, int redirectionCode) throws IOException {
+        if (cookie.isEmpty()) {
+            redirectWithoutCookie(url, dos, redirectionCode);
+        } else {
+            redirectWithCookie(url, dos, redirectionCode);
+        }
+    }
+
+    private void redirectWithCookie(String url, DataOutputStream dos, int redirectionCode) throws IOException {
+        if (redirectionCode == 301) {
+            response301Header(dos, url);
+        } else if (redirectionCode == 302) {
+            response302HeaderWithCookie(dos, url);
+        } else {
+            byte[] body = resourceHandler.getByteArray(url);
+            response404Header(dos, body.length);
+            responseBody(dos, body);
+        }
+    }
+
+    private void redirectWithoutCookie(String url, DataOutputStream dos, int redirectionCode) throws IOException {
         if (redirectionCode == 301) {
             response301Header(dos, url);
         } else if (redirectionCode == 302) {
@@ -85,12 +116,23 @@ public class Response {
         }
     }
 
-
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void response200HeaderWithCookie(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("Set-Cookie: SID=" + cookie + "; Path=/\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             logger.error(e.getMessage());
@@ -119,6 +161,18 @@ public class Response {
         }
     }
 
+    private void response302HeaderWithCookie(DataOutputStream dos, String newLocation) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: " + newLocation + "\r\n");
+            dos.writeBytes("Content-Length: 0\r\n");
+            dos.writeBytes("Set-Cookie: SID=" + cookie + "; Path=/\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
     private void response404Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
@@ -138,6 +192,4 @@ public class Response {
             logger.error(e.getMessage());
         }
     }
-
-
 }
