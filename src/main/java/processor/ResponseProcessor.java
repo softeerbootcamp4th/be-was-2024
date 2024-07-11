d6bb1df43a2f36a54bfc11f7ccbe1b4e60bf2ceb
DrRivaski <leesm990708@gmail.com>
2024-07-11 17:05:21 +0900

    [add] get response에 대한 메소드 추가 - GetDistributor에서 응답하는 모든 부분에 대한 메소드 추가 - 기본 응답 메소드 추가 - not found 응답 메소드 추가

--- src/main/java/processor/ResponseProcessor.java
+++ src/main/java/processor/ResponseProcessor.java
@@ -16,6 +16,26 @@ public class ResponseProcessor {
         return new ResponseProcessor(dos);
     }
 
+    public void defaultResponse(String path) throws IOException {
+        Response response = new Response.Builder()
+                .url(path)
+                .statusCode(200)
+                .dataOutputStream(dos)
+                .build();
+
+        response.sendResponse();
+    }
+
+    public void notFoundResponse() throws IOException {
+        Response response = new Response.Builder()
+                .url("/not_found.html")
+                .dataOutputStream(dos)
+                .redirectCode(404)
+                .build();
+
+        response.sendResponse();
+    }
+
     public void createUserResponse() throws IOException {
         Response response = new Response.Builder()
                 .url("/index.html")
@@ -42,6 +62,36 @@ public class ResponseProcessor {
                 .url("/login/login_failed.html")
                 .dataOutputStream(dos)
                 .redirectCode(404)
+                .build();
+
+        response.sendResponse();
+    }
+
+    public void logoutResponse() throws IOException {
+        Response response = new Response.Builder()
+                .url("/index.html")
+                .dataOutputStream(dos)
+                .redirectCode(302)
+                .build();
+
+        response.sendResponse();
+    }
+
+    public void loginSuccessWithSessionId() throws IOException {
+        Response response = new Response.Builder()
+                .url("/main/index.html")
+                .statusCode(200)
+                .dataOutputStream(dos)
+                .build();
+
+        response.sendResponse();
+    }
+
+    public void loginResponse(String path) throws IOException {
+        Response response = new Response.Builder()
+                .url(path)
+                .statusCode(200)
+                .dataOutputStream(dos)
                 .build();
 
         response.sendResponse();
