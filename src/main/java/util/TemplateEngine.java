d60c9363d3cfc0d2f031d717f0d28b03b3c2aba7
k000927 <kimsungjin927@gmail.com>
2024-07-11 15:17:18 +0900

    feature: html 파일을 동적으로 렌더링하는 renderTemplate 메소드 작성
    - 템플릿 원본은 렌더링할 부분을 {{key}} 형식으로 표현한다.

--- src/main/java/util/TemplateEngine.java
+++ src/main/java/util/TemplateEngine.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.util.Map;
+
 public class TemplateEngine {
 
     public static byte[] showAlert(String msg, String redirectUrl) {
@@ -16,5 +18,13 @@ public class TemplateEngine {
         return alert.getBytes();
     }
 
+    public static byte[] renderTemplate(byte[] template, Map<String, String> data) {
+        String rawTemplate = new String(template);
+        for (String key : data.keySet()) {
+            rawTemplate = rawTemplate.replace("{{" + key + "}}", data.get(key));
+        }
+        System.out.println("rawTemplate = " + rawTemplate);
+        return rawTemplate.getBytes();
+    }
 
 }
