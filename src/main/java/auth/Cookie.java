be171e8760106f2d106f7690bbcc9f36904229b3
mjmj <violet_prog@naver.com>
2024-07-11 11:00:02 +0900

    [feature] Cookie 객체 생성

---
+++ src/main/java/auth/Cookie.java
@@ -0,0 +1,46 @@
+package auth;
+
+public class Cookie {
+  public static final String SET_COOKIE = "Set-Cookie";
+  public static final String MAX_AGE = "maxAge";
+  public static final String PATH = "path";
+  private String key;
+  private String value;
+
+  private int maxAge;
+  private String path;
+
+  private Cookie() {}
+  public Cookie(String key, String value) {
+    this.key = key;
+    this.value = value;
+    this.maxAge=3600;
+    this.path="/";
+  }
+
+  public int getMaxAge() {
+    return maxAge;
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public Cookie maxAge(int maxAge) {
+    this.maxAge = maxAge;
+    return this;
+  }
+
+  public Cookie path(String path) {
+    this.path = path;
+    return this;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public String getValue() {
+    return value;
+  }
+}
