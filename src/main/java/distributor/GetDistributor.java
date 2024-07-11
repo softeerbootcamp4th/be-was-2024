5b752ee5d95cafb465a9d1295760ecf7985e9e36
DrRivaski <leesm990708@gmail.com>
2024-07-11 17:04:31 +0900

    [mod] ResponseProcessor 이용하도록 수정 - GetDistributor에서 응답하는 부분을 ResponseProcessor에게 모두 위임하도록 수정

--- src/main/java/distributor/GetDistributor.java
+++ src/main/java/distributor/GetDistributor.java
@@ -1,8 +1,8 @@
 package distributor;
 
 import handler.SessionHandler;
+import processor.ResponseProcessor;
 import webserver.Request;
-import webserver.Response;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -28,60 +28,47 @@ public class GetDistributor extends Distributor {
     private void processQuery(DataOutputStream dos) throws IOException {
         String path = request.getPath();
         if (path.equals("/user/create")) {
-            Response response = new Response.Builder()
-                    .url("/not_found.html")
-                    .dataOutputStream(dos)
-                    .redirectCode(404)
-                    .build();
-
-            response.sendResponse();
+            ResponseProcessor responseProcessor = ResponseProcessor.from(dos);
+            responseProcessor.notFoundResponse();
         }
     }
 
     private void processNonQuery(DataOutputStream dos) throws IOException {
         String path = request.getPath();
         if (path.equals("/logout")) {
-            // 세션 삭제
-            String userId = request.getSessionId();
-            SessionHandler.deleteSession(userId);
-
-            Response response = new Response.Builder()
-                    .url("/index.html")
-                    .dataOutputStream(dos)
-                    .redirectCode(302)
-                    .build();
-
-            response.sendResponse();
+            processLogout(dos);
         } else if (path.equals("/login/index.html")) {
-            String sessionId = request.getSessionId();
-
-            // 만약 세션아이디가 존재한다면 그냥 로그인 화면으로 이동
-            if (SessionHandler.verifySessionId(sessionId)) {
-                Response response = new Response.Builder()
-                        .url("/main/index.html")
-                        .statusCode(200)
-                        .dataOutputStream(dos)
-                        .build();
-
-                response.sendResponse();
-            } else {
-                // 세션아이디가 존재하지 않는다면 그대로
-                Response response = new Response.Builder()
-                        .url(path)
-                        .statusCode(200)
-                        .dataOutputStream(dos)
-                        .build();
-
-                response.sendResponse();
-            }
+            processLogin(dos, path);
         } else {
-            Response response = new Response.Builder()
-                    .url(path)
-                    .statusCode(200)
-                    .dataOutputStream(dos)
-                    .build();
+            processDefault(dos, path);
+        }
+    }
 
-            response.sendResponse();
+    private void processDefault(DataOutputStream dos, String path) throws IOException {
+        ResponseProcessor responseProcessor = ResponseProcessor.from(dos);
+        responseProcessor.defaultResponse(path);
+    }
+
+    private void processLogout(DataOutputStream dos) throws IOException {
+        // 세션 삭제
+        String userId = request.getSessionId();
+        SessionHandler.deleteSession(userId);
+
+        ResponseProcessor responseProcessor = ResponseProcessor.from(dos);
+        responseProcessor.logoutResponse();
+    }
+
+    private void processLogin(DataOutputStream dos, String path) throws IOException {
+        String sessionId = request.getSessionId();
+
+        // 만약 세션아이디가 존재한다면 그냥 로그인 화면으로 이동
+        if (SessionHandler.verifySessionId(sessionId)) {
+            ResponseProcessor responseProcessor = ResponseProcessor.from(dos);
+            responseProcessor.loginSuccessWithSessionId();
+        } else {
+            // 세션아이디가 존재하지 않는다면 그대로
+            ResponseProcessor responseProcessor = ResponseProcessor.from(dos);
+            responseProcessor.loginResponse(path);
         }
     }
 }
