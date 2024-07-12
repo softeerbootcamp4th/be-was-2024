f01e2422d56b109fa5cec985ceb321ec3ee5cf27
mjmj <violet_prog@naver.com>
2024-07-12 14:17:53 +0900

    [fix] 로그인 상태에 따른 홈 화면을 파일로 나누지 않고 동적 html을 이용하여 처리

--- src/main/java/ApiProcess/HomepageApiProcess.java
+++ src/main/java/ApiProcess/HomepageApiProcess.java
@@ -7,10 +7,6 @@ import webserver.Response;
 public class HomepageApiProcess implements ApiProcess {
     @Override
     public String process(Request request, Response response) {
-        if(AuthUtil.isLogin(request) != null) {
-            return "index_login";
-        } else {
-            return "index";
-        }
+        return "index";
     }
 }
