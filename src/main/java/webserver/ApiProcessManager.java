a8d60a711d6a24d5f59e87c61d3d8c850b76375d
mjmj <violet_prog@naver.com>
2024-07-12 14:14:27 +0900

    [refactoring] ApiProcessManager를 싱글톤 객체로 변경 [add] /user/list를 처리하는 로직 추가

--- src/main/java/webserver/ApiProcessManager.java
+++ src/main/java/webserver/ApiProcessManager.java
@@ -12,17 +12,26 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ApiProcessManager {
     private static Logger logger = LoggerFactory.getLogger(ApiProcessManager.class);
-    private static Map<ApiPair, ApiProcess> apiProcessStore = new ConcurrentHashMap<ApiPair, ApiProcess>();
+    private static Map<ApiPair, ApiProcess> apiProcessStore;
 
-    public ApiProcessManager() {
+    private static class LazyHolder {
+        private static ApiProcessManager instance = new ApiProcessManager();
+    }
+
+    public static ApiProcessManager getInstance() {
+        return LazyHolder.instance;
+    }
+
+    private ApiProcessManager() {
         // 로직 처리를 위한 구체 클래스를 설정한다. (RequestMapping에서 추상화 된 객체를 사용하기 위해 인터페이스 사용)
        apiProcessStore = new ConcurrentHashMap<ApiPair, ApiProcess>(){{
-           put(new ApiPair("/", HttpMethod.GET), new HomepageApiProcess());
-           put(new ApiPair("/registration", HttpMethod.GET), new RegisterpageApiProcess());
-           put(new ApiPair("/user/create", HttpMethod.POST), new RegisterApiProcess());
-           put(new ApiPair("/user/login", HttpMethod.POST), new LoginApiProcess());
-           put(new ApiPair("/login", HttpMethod.GET), new LoginPageApiProcess());
-           put(new ApiPair("/user/logout", HttpMethod.POST), new LogoutApiProcess());
+           put(ApiPair.of("/", HttpMethod.GET), new HomepageApiProcess());
+           put(ApiPair.of("/registration", HttpMethod.GET), new RegisterpageApiProcess());
+           put(ApiPair.of("/user/create", HttpMethod.POST), new RegisterApiProcess());
+           put(ApiPair.of("/user/login", HttpMethod.POST), new LoginApiProcess());
+           put(ApiPair.of("/login", HttpMethod.GET), new LoginPageApiProcess());
+           put(ApiPair.of("/user/logout", HttpMethod.POST), new LogoutApiProcess());
+           put(ApiPair.of("/user/list", HttpMethod.GET), new UserlistPageApiProcess());
        }};
     }
 
@@ -35,6 +44,6 @@ public class ApiProcessManager {
            return new StaticApiProcess();
         }
 
-        return apiProcessStore.getOrDefault(new ApiPair(path, method), new NotFoundApiProcess());
+        return apiProcessStore.getOrDefault(ApiPair.of(path, method), new NotFoundApiProcess());
     }
 }
