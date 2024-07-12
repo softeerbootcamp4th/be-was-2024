b62b1bb71b518e4a08239f995193e6683a83f83a
blaxsior <heejun2798@gmail.com>
2024-07-12 09:54:00 +0900

    refactor: ItemFinder이 중간 item이 맵 자료구조인 경우에도 값을 가져올 수 있도록 개선

--- src/main/java/view/ItemFinder.java
+++ src/main/java/view/ItemFinder.java
@@ -20,35 +20,49 @@ public class ItemFinder {
      * @return 찾고 있는 아이템
      */
     public String getItemString(String itemName) {
-
-        // 첫번째에는 map에서 찾기. 두번째부터는 리플렉션으로 값이 존재하는지 탐색
-        String[] itemSegments = itemName.split("\\.", 2);
-        if (itemSegments.length == 0) throw new TemplateException(ITEM_NAME_INVALID);
-
-        // 아이템 획특
-        Object item = items.get(itemSegments[0]);
-
-        if(itemSegments.length == 2) item = getItem(item, itemSegments[1]);
+        Object item = getItem(this.items, itemName);
 
         return item != null ? item.toString() : null;
     }
 
+    /**
+     * 객체로부터 이름에 지정된 값을 얻는 메서드.
+     * @param sourceItem 대상 아이템을 탐색할 루트 아이템
+     * @param itemName 찾고 싶은 아이템의 이름
+     * @return 찾는 대상 아이템
+     */
     public static Object getItem(Object sourceItem, String itemName) {
         Object item = sourceItem;
         String[] itemSegments = itemName.split("\\.");
 
         for (String itemSegment : itemSegments) {
+            // 찾지 못했는데 null을 던지면, 대상 필드가 없는 것인지 / 아니면 대상 필드는 있는데 null 값인지 구분 어려움
+            // 만약 나중에 이로 인한 불편함이 생기면 수정 예정
             if (item == null) throw new TemplateException(ITEM_NOT_FOUND);
-            Class<?> objClass = item.getClass();
 
-            try {
-                Field targetField = objClass.getDeclaredField(itemSegment);
-                targetField.setAccessible(true);
-                item = targetField.get(item);
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                throw new TemplateException(ITEM_NOT_FOUND, e);
-            }
+            if (item instanceof Map<?,?>) item = getNextItemFromMap(item, itemSegment);
+            else item = getNextItemFromObject(item, itemSegment);
         }
         return item;
+    }
+
+    private static Object getNextItemFromMap(Object sourceItem, String itemSegment) {
+        try {
+            return ((Map<?,?>) sourceItem).get(itemSegment);
+        } catch(Exception e) {
+            throw new TemplateException(ITEM_NOT_FOUND, e);
+        }
+    }
+
+    private static Object getNextItemFromObject(Object sourceItem, String itemSegment) {
+        Class<?> objClass = sourceItem.getClass();
+
+        try {
+            Field targetField = objClass.getDeclaredField(itemSegment);
+            targetField.setAccessible(true);
+            return targetField.get(sourceItem);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new TemplateException(ITEM_NOT_FOUND, e);
+        }
     }
 }
