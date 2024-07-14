5f27cb6e4a14c86990d5d2e0e49599f719c412e7
hyeokson <chanhuk1765@gmail.com>
2024-07-14 19:49:13 +0900

    feat: cookie 인터페이스 생성 / RedirectCookie 생성
    - 핸들러에서 작업 성공 시, 쿠키에 저장된 이전 요청 url으로 redirect 할 때 사용하는 RedirectCookie를 생성

--- src/main/java/cookie/Cookie.java
+++ src/main/java/cookie/Cookie.java
@@ -1,67 +1,8 @@
 package cookie;
 
-public class Cookie {
-    private static final String headerName = "Set-Cookie";
-    private static final String CRLF = "\r\n";
+public interface Cookie {
+    static final String headerName = "Set-Cookie";
+    static final String CRLF = "\r\n";
 
-    private String sessionId;
-    private String domain;
-    private String path;
-    private String maxAge;
-    private boolean secure;
-    private boolean httpOnly;
-
-    public Cookie(String sessionId){
-        this.sessionId = sessionId;
-        domain = "localhost";
-        path = "/";
-        secure = false;
-        httpOnly = false;
-    }
-
-    public void setSessionId(String sessionId) {
-        this.sessionId = sessionId;
-    }
-
-    public void setDomain(String domain) {
-        this.domain = domain;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public void setMaxAge(int maxAge) {
-        this.maxAge = String.valueOf(maxAge);
-    }
-
-
-    public void setSecure(boolean secure) {
-        this.secure = secure;
-    }
-
-    public void setHttpOnly(boolean httpOnly) {
-        this.httpOnly = httpOnly;
-    }
-
-    public String getCookieString(){
-        StringBuilder sb = new StringBuilder();
-        sb.append(headerName + ": ")
-                .append(CookieAttribute.SESSION_ID.getAttributeName()).append("=").append(sessionId).append(";")
-                .append(CookieAttribute.PATH.getAttributeName()).append("=").append(path).append(";")
-                .append(CookieAttribute.DOMAIN.getAttributeName()).append("=").append(domain).append(";");
-        if(maxAge!=null)
-            sb.append(CookieAttribute.MAX_AGE.getAttributeName()).append("=").append(maxAge).append(";");
-
-        if(secure)
-            sb.append(CookieAttribute.SECURE.getAttributeName()).append(";");
-
-        if(httpOnly)
-            sb.append(CookieAttribute.HTTPONLY.getAttributeName()).append(";");
-
-        sb.append(CRLF);
-
-        return sb.toString();
-    }
-
+    public String getCookieString();
 }
---
+++ src/main/java/cookie/RedirectCookie.java
@@ -0,0 +1,45 @@
+package cookie;
+
+// 핸들러에서 작업 성공 시, redirect 해야 하는 url을 설정하는 쿠키
+public class RedirectCookie implements Cookie{
+    private final String redirectUrl;
+    private final String domain;
+    private final String path;
+    private String maxAge;
+    private final boolean secure;
+    private final boolean httpOnly;
+
+    public RedirectCookie(String redirectUrl) {
+        this.redirectUrl = redirectUrl;
+        domain = "localhost";
+        path = "/";
+        secure = false;
+        httpOnly = false;
+    }
+
+    public void setMaxAge(int maxAge) {
+        this.maxAge = String.valueOf(maxAge);
+    }
+
+    public String getCookieString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(headerName + ": ")
+                .append(CookieAttribute.REDIRECT.getAttributeName()).append("=").append(redirectUrl).append("; ")
+                .append(CookieAttribute.PATH.getAttributeName()).append("=").append(path).append("; ")
+                .append(CookieAttribute.DOMAIN.getAttributeName()).append("=").append(domain).append("; ");
+
+        if(maxAge!=null)
+            sb.append(CookieAttribute.MAX_AGE.getAttributeName()).append("=").append(maxAge).append("; ");
+
+        if(secure)
+            sb.append(CookieAttribute.SECURE.getAttributeName()).append("; ");
+
+        if(httpOnly)
+            sb.append(CookieAttribute.HTTPONLY.getAttributeName()).append("; ");
+
+        sb.append(CRLF);
+
+        return sb.toString();
+    }
+}
