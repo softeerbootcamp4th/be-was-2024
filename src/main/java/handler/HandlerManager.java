d2fd734d4a57d430574188e62adb43b82420b188
hyeokson <chanhuk1765@gmail.com>
2024-07-14 20:15:20 +0900

    refactor: 기본 화면 경로를 "/"로 변경 / redirect 쿠키를 사용하도록 변경
    - 사용자 목록 버튼을 누른 후 로그인 화면으로 넘어갔다면, redirect 쿠키를 사용하여 로그인 성공시에 기본화면이 아니라 사용자 목록 화면으로 redirection하도록 변경

--- src/main/java/handler/HandlerManager.java
+++ src/main/java/handler/HandlerManager.java
@@ -5,15 +5,19 @@ import constant.HttpMethod;
 import constant.HttpStatus;
 import constant.MimeType;
 import cookie.Cookie;
+import cookie.RedirectCookie;
+import cookie.SessionCookie;
 import db.Database;
 import dto.HttpRequest;
 import dto.HttpResponse;
 import exception.InvalidHttpRequestException;
 import exception.ResourceNotFoundException;
+import model.MyTagDomain;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import session.Session;
+import util.DynamicFileBuilder;
 import util.HttpRequestParser;
 
 import java.io.*;
@@ -41,6 +45,28 @@ public class HandlerManager {
         }
 
         // 각 HttpRequest를 처리하는 Handler 등록
+        handlers.get(HttpMethod.GET).put("/", (httpRequest, httpResponse) -> {
+
+            Map<String, List<MyTagDomain>> model = new HashMap<>();
+
+            // 쿠키에 있는 sessionId가 유효한지 검사
+            if(httpRequest.getSessionId().isPresent()){
+                String sessionId = httpRequest.getSessionId().get();
+                String userId = Session.getUserId(sessionId);
+                if(userId != null && Database.userExists(userId)){
+                    User user = Database.findUserById(userId);
+                    List<User> userList = new ArrayList<>();
+                    userList.add(user);
+                    model.put("login", new ArrayList<>(userList));
+                    // 동적 index.html 반환
+                    DynamicFileBuilder.setHttpResponse(httpResponse, "/index.html", model);
+                    return;
+                }
+            }
+
+            // 쿠키에 세션 정보가 없거나 유효하지 않으면 빈 model 전달
+            DynamicFileBuilder.setHttpResponse(httpResponse, "/index.html", model);
+        });
 
         // 회원가입 handler
         handlers.get(HttpMethod.POST).put("/user/create", (httpRequest, httpResponse) -> {
@@ -52,7 +78,7 @@ public class HandlerManager {
             Database.addUser(user);
 
             // 302 응답 생성
-            httpResponse.setRedirect("/index.html");
+            httpResponse.setRedirect("/");
         });
 
         // 로그인 처리 handler
@@ -74,8 +100,20 @@ public class HandlerManager {
                 // 로그인 성공 시, /index.html로 redirect
                 if(user.getPassword().equals(password)){
                     String sessionId = Session.createSession(userId);
-                    httpResponse.setCookie(new Cookie(sessionId));
-                    httpResponse.setRedirect("/index.html");
+                    httpResponse.setCookie(new SessionCookie(sessionId));
+
+                    // 로그인을 하기 전에 유저 리스트 버튼을 눌렀다면
+                    // /user/list로 리다이렉트 하고, redirect 쿠키를 삭제
+                    if(httpRequest.getRedirectUrl().isPresent() &&
+                        httpRequest.getRedirectUrl().get().equals("/user/list")){
+
+                        RedirectCookie cookie = new RedirectCookie("/user/list");
+                        cookie.setMaxAge(0);
+                        httpResponse.setCookie(cookie);
+                        httpResponse.setRedirect("/user/list");
+                    }
+                    else
+                        httpResponse.setRedirect("/");
                     return;
                 }
             }
@@ -92,13 +130,38 @@ public class HandlerManager {
                 String sessionId = httpRequest.getSessionId().get();
                 Session.deleteSession(sessionId);
 
-                Cookie cookie = new Cookie(sessionId);
-                cookie.setMaxAge(0);
-                httpResponse.setCookie(cookie);
+                // 세션 쿠키 삭제
+                SessionCookie sessionCookie = new SessionCookie(sessionId);
+                sessionCookie.setMaxAge(0);
+                httpResponse.setCookie(sessionCookie);
             }
 
-            httpResponse.setRedirect("/index.html");
+            httpResponse.setRedirect("/");
 
+        });
+
+        handlers.get(HttpMethod.GET).put("/user/list", (httpRequest, httpResponse) -> {
+            Map<String, List<MyTagDomain>> model = new HashMap<>();
+
+            // 쿠키에 있는 sessionId가 유효한지 검사
+            if(httpRequest.getSessionId().isPresent()){
+                String sessionId = httpRequest.getSessionId().get();
+                String userId = Session.getUserId(sessionId);
+                if(userId != null && Database.userExists(userId)){
+                    List<User> users = Database.findAllByList();
+                    List<MyTagDomain> userList = new ArrayList<>(users);
+                    model.put("userlist", userList);
+                    // 동적 index.html 반환
+                    DynamicFileBuilder.setHttpResponse(httpResponse, "/user/list.html", model);
+                    return;
+                }
+            }
+
+            // 쿠키에 세션 정보가 없거나 유효하지 않으면 로그인 페이지로 이동
+            // 로그인 성공 시, /user/list로 redirect 할 수 있도록 redirect cookie 설정
+            RedirectCookie cookie = new RedirectCookie("/user/list");
+            httpResponse.setCookie(cookie);
+            httpResponse.setRedirect("/login/index.html");
         });
     }
 
@@ -154,7 +217,7 @@ public class HandlerManager {
     }
 
     // 정적 파일 응답 메서드
-    public void handleStaticResource(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, IllegalArgumentException {
+    public void handleStaticResource(HttpRequest httpRequest, HttpResponse httpResponse) throws IllegalArgumentException {
 
         byte[] body = readStaticFile(httpRequest.getPath().orElseThrow(
                 () -> new InvalidHttpRequestException("invalid path")));
@@ -171,10 +234,7 @@ public class HandlerManager {
         }
         else{
             // url에 해당하는 파일이 없으면 404 error 응답
-            httpResponse.setHttpStatus(HttpStatus.NOT_FOUND);
-            httpResponse.addHeader(CONTENT_TYPE, FileExtensionType.valueOf(extensionType).getContentType());
-            httpResponse.addHeader(CONTENT_LENGTH, String.valueOf(ERROR_MESSAGE_404.length()));
-            httpResponse.setBody(ERROR_MESSAGE_404.getBytes(CHARSET_UTF8));
+            httpResponse.setErrorResponse(HttpStatus.NOT_FOUND);
         }
     }
 
