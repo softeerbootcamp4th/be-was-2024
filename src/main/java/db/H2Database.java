a8c90fdb6be2b1cec96678f6c226627cd81e223c
ysndy <ysndy@naver.com>
2024-07-16 18:22:32 +0900

    feat: h2 db 구현

---
+++ src/main/java/db/H2Database.java
@@ -0,0 +1,25 @@
+package db;
+
+import java.sql.*;
+
+public class H2Database {
+
+    private static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ResultSet select(String sql) throws SQLException {
+        return connection.createStatement().executeQuery(sql);
+    }
+
+    public static void insert(String sql) throws SQLException {
+        connection.createStatement().execute(sql);
+    }
+
+}
