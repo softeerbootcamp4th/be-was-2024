efae22fc4e3d8df72ef6fa96522df0af5a7092ef
wjddn2165 <qowjddn2165@gmail.com>
2024-07-16 11:24:22 +0900

    기존에 각 Handler에서 응답까지 전부 처리하던 로직을 RequestHandler에서 응답을 처리하도록 변경.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,21 +2,24 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+
+import enums.Status;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.Handler;
 import utils.HttpRequestParser;
 import utils.HttpResponseHandler;
+import utils.Model;
+import view.View;
+import view.ViewResolver;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-
+    private static final String STATIC_RESOURCE_PATH = "src/main/resources/static";
     private Socket connection;
-
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
-
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
@@ -35,9 +38,34 @@ public class RequestHandler implements Runnable {
             String url = httpRequestParser.getUrl();
             logger.info("요청 URL: " + url);
 
+            // 정적 리소스 요청 처리
+            if (httpRequestParser.getExtension() != null) {
+                StaticResourceHandler staticResourceHandler = new StaticResourceHandler(STATIC_RESOURCE_PATH);
+                staticResourceHandler.handle(httpRequestParser, httpResponseHandler);
+                return;
+            }
+
+            // 뷰 처리
+            Model model = new Model();
+            ViewResolver viewResolver = new ViewResolver(STATIC_RESOURCE_PATH, ".html");
+
             Router router = new Router();
             Handler handler = router.getHandler(httpRequestParser);
-            handler.handle(httpRequestParser, httpResponseHandler);
+            String viewName = handler.handle(httpRequestParser, httpResponseHandler, model);
+
+            if (viewName.startsWith("redirect:")) {
+                String redirectUrl = viewName.substring("redirect:".length());
+                httpResponseHandler
+                        .addHeader("Location", redirectUrl)
+                        .respond();
+            } else {
+                View view = viewResolver.resolve(viewName);
+                String content = view.render(model.getAttributes());
+                httpResponseHandler
+                        .setBody(content.getBytes())
+                        .addHeader("Content-Type", "text/html")
+                        .respond();
+            }
 
         } catch (IOException e) {
             logger.error(e.getMessage());
