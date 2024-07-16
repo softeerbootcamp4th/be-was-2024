3805b4a5839aa3585933d2ac7edf94d4709a16f3
repitor222 <repitor15@gmail.com>
2024-07-16 10:31:23 +0900

    [feat] User의 로그인을 위한 Id, password 만을 이용한 생성자 생성
    Login 시 Id, Password 입력만을 위한 생성자 작성

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -13,6 +13,11 @@ public class User {
         this.email = email;
     }
 
+    public User(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
+
     public String getUserId() {
         return userId;
     }
