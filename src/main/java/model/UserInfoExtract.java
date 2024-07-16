e752e9782cf32626a8f70e095edbc99652e785ab
repitor222 <repitor15@gmail.com>
2024-07-16 10:30:45 +0900

    [feat] UserInfoExtract 에 create와 login을 위한 메소드 분리 및 반환타입 수정
    Map이 아닌 User 객체 반환하도록 수정, null값 반환 처리를 위한 메소드 분리

--- src/main/java/model/UserInfoExtract.java
+++ src/main/java/model/UserInfoExtract.java
@@ -5,14 +5,40 @@ import java.util.Map;
 
 public class UserInfoExtract {
 
-    public static Map<String, String> extractUserInfoFromBody(String body) {
+    public static User extractUserInfoFromBodyForCreate(String body) {
         String[] params = body.split("&");
         Map<String, String> userInfo = new HashMap<>();
         for (String param : params) {
             String[] keyValue = param.split("=");
             userInfo.put(keyValue[0], keyValue[1]);
         }
-        return userInfo;
+        String userId = userInfo.get("userId");
+        String password = userInfo.get("password");
+        String name = userInfo.get("name");
+        String email = userInfo.get("email");
+
+        User user = null;
+        if (userId != null && password != null && name != null && email != null) {
+            user = new User(userId, password, name, email);
+        }
+        return user;
+    }
+
+    public static User extractUserInfoFromBodyForLogin(String body) {
+        String[] params = body.split("&");
+        Map<String, String> userInfo = new HashMap<>();
+        for (String param : params) {
+            String[] keyValue = param.split("=");
+            userInfo.put(keyValue[0], keyValue[1]);
+        }
+        String userId = userInfo.get("userId");
+        String password = userInfo.get("password");
+
+        User user = null;
+        if (userId != null && password != null) {
+            user = new User(userId, password);
+        }
+        return user;
     }
 
     public static String extractSessionIdFromHeader(String header){
