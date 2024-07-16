a7171840d281873f4f43b53fbe69fb43b9c21209
ysndy <ysndy@naver.com>
2024-07-16 13:35:26 +0900

    fix: 제목 필드 추가

--- src/main/java/model/Post.java
+++ src/main/java/model/Post.java
@@ -2,8 +2,15 @@ package model;
 
 public class Post {
 
-    private String content;
     private String authorName;
+    private String title;
+    private String content;
+
+    public Post(String content, String title, String authorName) {
+        this.authorName = authorName;
+        this.title = title;
+        this.content = content;
+    }
 
     public String getAuthorName() {
         return authorName;
@@ -11,5 +18,14 @@ public class Post {
 
     public String getContent() {
         return content;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "authorName:"+authorName+", title:"+title+", content:"+content;
     }
 }
