f5aa1c9f4635f64b8cdb853160386c497a1e3ff5
repitor222 <repitor15@gmail.com>
2024-07-16 10:51:56 +0900

    [feat] HttpStatus Enum 타입 생성
    RequestResponse 간결화를 위한 HttpStatus Enum 타입 생성

---
+++ src/main/java/webserver/enumPackage/HttpStatus.java
@@ -0,0 +1,28 @@
+package webserver.enumPackage;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    FOUND(302, "Found"),
+    NOT_FOUND(404, "Not Found");
+
+    private final int code;
+    private final String message;
+
+    HttpStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return code + " " + message;
+    }
+}
