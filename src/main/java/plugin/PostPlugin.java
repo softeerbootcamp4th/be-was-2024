aeed7be52b3390ed2370518a52f509a09a10319c
ysndy <ysndy@naver.com>
2024-07-17 15:27:04 +0900

    doc: javadoc 적용

--- src/main/java/plugin/PostPlugin.java
+++ src/main/java/plugin/PostPlugin.java
@@ -11,9 +11,18 @@ import webserver.http.response.Status;
 import java.sql.SQLException;
 import java.util.Map;
 
+/**
+ * 게시글과 관련된 처리를 하는 클래스
+ */
 @Plugin
 public class PostPlugin {
 
+    /**
+     * post 로 요청이 왔을 때 게시글 업로드를 처리하는 메소드
+     * @param request
+     * @return
+     * @throws SQLException
+     */
     @Post(path = "/post")
     public Response post(Request request) throws SQLException {
 
