0657c7cdf2bf65328719993f9ff6304d526317ef
repitor222 <repitor15@gmail.com>
2024-07-18 10:45:58 +0900

    [feat] 들어온 이미지에 랜덤한 파일 이름을 설정한 뒤 저장 설정하는 ImageSaver 클래스 생성
    랜덤한 UUID이름을 지정하여 파일 저장

--- src/main/java/webserver/ImageSaver.java
+++ src/main/java/webserver/ImageSaver.java
@@ -3,17 +3,19 @@ package webserver;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 public class ImageSaver {
 
     // 이미지 바이트 배열을 파일로 저장하는 메서드
-    public static void saveImage(byte[] imageBytes, String directory, String fileName) {
+    public static String saveImage(byte[] imageBytes, String directory) {
+        String fileName = UUID.randomUUID().toString();
         String filePath = directory + "/" + fileName;
         try (FileOutputStream fos = new FileOutputStream(filePath)) {
             fos.write(imageBytes);
-            System.out.println("이미지 파일이 성공적으로 저장되었습니다.");
         } catch (IOException e) {
-            System.err.println("이미지 파일 저장 중 오류가 발생했습니다: " + e.getMessage());
+            throw new RuntimeException(e);
         }
+        return fileName;
     }
 }
