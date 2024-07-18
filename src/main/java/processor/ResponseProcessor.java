8a8fb9c90e3d35bb8b0f8788a37830e7fa3d421c
DrRivaski <leesm990708@gmail.com>
2024-07-18 17:46:10 +0900

    [chore] 변수 인라인화

--- src/main/java/processor/ResponseProcessor.java
+++ src/main/java/processor/ResponseProcessor.java
@@ -4,115 +4,91 @@ import model.ViewData;
 
 public class ResponseProcessor {
     public ViewData defaultResponse(String path, String sessionId) {
-        ViewData viewData = new ViewData.Builder()
+        return new ViewData.Builder()
                 .url(path)
                 .statusCode(200)
                 .cookie(sessionId)
                 .build();
-
-        return viewData;
     }
 
     public ViewData notFoundResponse() {
-        ViewData viewData = new ViewData.Builder()
+        return new ViewData.Builder()
                 .url("/not_found.html")
                 .statusCode(404)
                 .build();
-
-        return viewData;
     }
 
     public ViewData createUserResponse(String sessionId) {
-        ViewData viewData = new ViewData.Builder()
+        return new ViewData.Builder()
                 .url("/index.html")
                 .redirectCode(302)
                 .cookie(sessionId)
                 .build();
-
-        return viewData;
     }
 
     public ViewData loginSuccessResponse(String sessionId) {
-        ViewData viewData = new ViewData.Builder()
+        return new ViewData.Builder()
                 .url("/index.html")
                 .cookie(sessionId)
                 .redirectCode(302)
                 .build();
-
-        return viewData;
     }
 
     public ViewData loginFailedResponse() {
-        ViewData viewData = new ViewData.Builder()
+        return new ViewData.Builder()
                 .url("/login/login_failed.html")
                 .statusCode(404)
                 .build();
-
-        return viewData;
     }
 
     public ViewData logoutResponse() {
-        ViewData viewData = new ViewData.Builder()
+        return new ViewData.Builder()
                 .url("/index.html")
                 .redirectCode(302)
                 .build();
-
-        return viewData;
     }
 
     public ViewData loginSuccessWithSessionId(String sessionId) {
-        ViewData viewData = new ViewData.Builder()
+        return new ViewData.Builder()
                 .url("/index.html")
                 .statusCode(200)
                 .cookie(sessionId)
                 .build();
-
-        return viewData;
     }
 
     public ViewData loginResponse(String path, String sessionId) {
-        ViewData viewData = new ViewData.Builder()
+        return new ViewData.Builder()
                 .url(path)
                 .statusCode(200)
                 .cookie(sessionId)
                 .build();
-
-        return viewData;
     }
 
     public ViewData userListResponse() {
-        ViewData viewData = new ViewData.Builder()
+        return new ViewData.Builder()
                 .url("/user/list.html")
                 .statusCode(200)
                 .build();
-
-        return viewData;
     }
 
     public ViewData unauthorizedUserListResponse() {
-        ViewData viewData = new ViewData.Builder()
+        return new ViewData.Builder()
                 .url("/index.html")
                 .redirectCode(302)
                 .build();
-
-        return viewData;
     }
 
     public ViewData writeResponse() {
-        ViewData viewData = new ViewData.Builder()
+        return new ViewData.Builder()
                 .url("/write/index.html")
                 .statusCode(200)
                 .build();
-
-        return viewData;
     }
 
     public ViewData unauthorizedWriteResponse() {
-        ViewData viewData = new ViewData.Builder()
+        return new ViewData.Builder()
                 .url("/login/index.html")
                 .redirectCode(302)
                 .build();
-
-        return viewData;
     }
 }
