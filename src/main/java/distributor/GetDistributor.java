f2920e0cd7097a7260a2f984e7e41b9b973df37d
DrRivaski <leesm990708@gmail.com>
2024-07-18 13:53:58 +0900

    [add] 사용자 리스트 반환하는 메소드 추가 - 쿠키를 이용하여 로그인된 상태와 로그인되지 않은 상태의 반환값 다르게 구현

--- src/main/java/distributor/GetDistributor.java
+++ src/main/java/distributor/GetDistributor.java
@@ -36,13 +36,14 @@ public class GetDistributor extends Distributor {
             processLogout();
         } else if (path.equals("/login/index.html")) {
             processLogin(path);
+        } else if (path.equals("/user/list")) {
+            processUserList();
         } else {
             processDefault(path);
         }
     }
 
     private void processDefault(String path) {
-        System.out.println("process default = " + path);
         ResponseProcessor responseProcessor = new ResponseProcessor();
         this.viewData = responseProcessor.defaultResponse(path, request.getSessionId());
     }
@@ -58,15 +59,25 @@ public class GetDistributor extends Distributor {
 
     private void processLogin(String path) {
         String sessionId = request.getSessionId();
+        ResponseProcessor responseProcessor = new ResponseProcessor();
 
         // 만약 세션아이디가 존재한다면 그냥 로그인 화면으로 이동
         if (SessionHandler.verifySessionId(sessionId)) {
-            ResponseProcessor responseProcessor = new ResponseProcessor();
             this.viewData = responseProcessor.loginSuccessWithSessionId(sessionId);
         } else {
             // 세션아이디가 존재하지 않는다면 그대로
-            ResponseProcessor responseProcessor = new ResponseProcessor();
             this.viewData = responseProcessor.loginResponse(path, sessionId);
+        }
+    }
+
+    private void processUserList() {
+        String sessionId = request.getSessionId();
+        ResponseProcessor responseProcessor = new ResponseProcessor();
+
+        if (SessionHandler.verifySessionId(sessionId)) {
+            this.viewData = responseProcessor.userListResponse();
+        } else {
+            this.viewData = responseProcessor.unauthorizedUserListResponse();
         }
     }
 
