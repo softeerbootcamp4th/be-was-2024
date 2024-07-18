645b8e0989ef563395effbca8f9aa9aecce9ee5f
DrRivaski <leesm990708@gmail.com>
2024-07-18 11:16:43 +0900

    [mod] URLParser 이름 변경에 따른 수정 - RequestLineUtil로 이름 변경

--- src/test/java/webserver/RequestLineUtilTest.java
+++ src/test/java/webserver/RequestLineUtilTest.java
@@ -3,24 +3,23 @@ package webserver;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import utils.RequestLineUtil;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class URLParserTest {
-    URLParser urlParser = new URLParser();
+class RequestLineUtilTest {
+    RequestLineUtil requestLineUtil = new RequestLineUtil();
     String line = "GET /index.html HTTP/1.1";
 
     @Test
     @DisplayName("get url 테스트")
     void testGetURL() {
-        String url = urlParser.getURL(line);
+        String url = requestLineUtil.getURL(line);
         Assertions.assertThat(url).isEqualTo("/index.html");
     }
 
     @Test
     @DisplayName("http 메소드 테스트")
     void testGetHttpMethod() {
-        String method = urlParser.getHttpMethod(line);
+        String method = requestLineUtil.getHttpMethod(line);
         Assertions.assertThat(method).isEqualTo("GET");
     }
 }
