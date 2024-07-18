9705d42e66f11c9050a2ef147d165a9dd63e616a
DrRivaski <leesm990708@gmail.com>
2024-07-18 15:14:00 +0900

    [mod] 글쓰기 버튼이 항상 보이도록 동적으로 작성하지 않도록 수정

--- src/main/java/builder/HtmlBuilder.java
+++ src/main/java/builder/HtmlBuilder.java
@@ -10,28 +10,25 @@ public class HtmlBuilder {
         ResourceUtil resourceUtil = new ResourceUtil();
         String template = new String(resourceUtil.getByteArray(templateFilePath));
 
-        String loginButtonHtml, registrationButtonText, registrationButtonHref, userNameHtml, postButtonHtml;
+        String loginButtonHtml, registrationButtonText, registrationButtonHref, userNameHtml;
 
         if (isLoggedIn) {
             loginButtonHtml = ""; // 로그인된 상태에서는 로그인 버튼을 숨김
             registrationButtonText = "로그아웃";
             registrationButtonHref = "/logout";
-            userNameHtml = "<p class=\"user-name\">" + username + "</p>";
-            postButtonHtml = "<li class=\"nav__menu__item\"><a class=\"btn btn_ghost btn_size_m\" href=\"/article\">글쓰기</a></li>";
+            userNameHtml = "<p class=\"user-name\">" + username + " 님</p>";
         } else {
             loginButtonHtml = "<li class=\"header__menu__item\"><a class=\"btn btn_contained btn_size_s\" href=\"/login\">로그인</a></li>";
             registrationButtonText = "회원가입";
             registrationButtonHref = "/registration";
             userNameHtml = "";
-            postButtonHtml = "";
         }
 
         // 문자열 대체
         template = template.replace("{username_placeholder}", userNameHtml)
                 .replace("{login_button_placeholder}", loginButtonHtml)
                 .replace("{registration_button_text}", registrationButtonText)
-                .replace("{registration_button_href}", registrationButtonHref)
-                .replace("{post_button_placeholder}", postButtonHtml);
+                .replace("{registration_button_href}", registrationButtonHref);
 
         return template;
     }
