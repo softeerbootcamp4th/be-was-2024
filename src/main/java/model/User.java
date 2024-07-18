83369784c700acbefc6c280944b3fb90365ec46c
DrRivaski <leesm990708@gmail.com>
2024-07-18 17:39:01 +0900

    [add] 빌더패턴을 이용한 생성자 추가

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -15,8 +15,46 @@ public class User {
         this.email = email;
     }
 
+    private User(Builder builder) {
+        this.userId = builder.userId;
+        this.password = builder.password;
+        this.name = builder.username;
+        this.email = builder.email;
+    }
+
     public static User from(HashMap<String, String> data) {
         return new User(data.get("userId"), data.get("password"), data.get("name"), data.get("email"));
+    }
+
+    public static class Builder {
+        private String userId;
+        private String password;
+        private String username;
+        private String email;
+
+        public Builder userId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
     }
 
     public String getUserId() {
