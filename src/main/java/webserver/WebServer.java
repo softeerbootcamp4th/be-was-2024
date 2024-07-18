ed8c4c52efd9f4066c88d50ffec7df409919b349
eckrin <bsh51515@naver.com>
2024-07-18 13:14:11 +0900

    fix: webserver thread-safe하게 수정

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -16,6 +16,8 @@ public class WebServer {
     private static final int DEFAULT_PORT = 8080;
     private static final int THREAD_POOL_SIZE = 200; // 톰캣(Tomcat 9 기준)의 default max pool size는 200
 
+    private static final ThreadLocal<ServerSocket> listenSocket = new ThreadLocal<>();
+
     public static void main(String args[]) throws Exception {
         int port = 0;
         if (args == null || args.length == 0) {
@@ -27,18 +29,20 @@ public class WebServer {
         ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
-        try (ServerSocket listenSocket = new ServerSocket(port)) {
+        try {
+            listenSocket.set(new ServerSocket(port));
             logger.info("Web Application Server started {} port.", port);
 
             // 클라이언트가 연결될 때까지 대기한다.
             Socket connection;
             RequestHandler requestHandler = RequestHandler.getInstance();
-            while((connection = listenSocket.accept()) != null) {
+            while((connection = listenSocket.get().accept()) != null) {
                 executeRequestHandlerThread(threadPool, connection, requestHandler);
             }
         } catch (IOException e) {
             logger.error("Server Start Error: " + e.getMessage());
         } finally {
+            listenSocket.get().close();
             threadPool.shutdown();
         }
     }
