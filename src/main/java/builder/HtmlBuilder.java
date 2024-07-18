aabde1a55e12b9090c9cc285fd12981126c52b97
DrRivaski <leesm990708@gmail.com>
2024-07-19 01:31:16 +0900

    [mod] 작성한 글의 제목 목록을 데이터베이스에서 조회하여 동적으로 HTML을 생성하도록 수정 - 로그인 상태와 로그인되지 않은 상태에 대해 처리하기 위해 메소드 분리

--- src/main/java/builder/HtmlBuilder.java
+++ src/main/java/builder/HtmlBuilder.java
@@ -1,37 +1,75 @@
 package builder;
 
+import db.PostDatabase;
 import db.UserDatabase;
+import handler.SessionHandler;
 import model.User;
 import utils.ResourceUtil;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
 
 public class HtmlBuilder {
-    public String generateIndexHtml(boolean isLoggedIn, String username) throws IOException {
+    public String generateIndexHtml(boolean isLoggedIn, String sessionId) throws IOException {
+        if (isLoggedIn) {
+            return generateLogInIndexHtml(sessionId);
+        } else {
+            return generateDefaultIndexHtml();
+        }
+    }
+
+    private String generateLogInIndexHtml(String sessionId) throws IOException {
         String templateFilePath = "/index.html"; // 템플릿 파일 경로
         ResourceUtil resourceUtil = new ResourceUtil();
         String template = new String(resourceUtil.getByteArray(templateFilePath));
 
+        User user = SessionHandler.getUser(sessionId);
+        String userId = user.getUserId();
+        String username = user.getName();
+
         String loginButtonHtml, registrationButtonText, registrationButtonHref, userNameHtml;
 
-        if (isLoggedIn) {
-            loginButtonHtml = ""; // 로그인된 상태에서는 로그인 버튼을 숨김
-            registrationButtonText = "로그아웃";
-            registrationButtonHref = "/logout";
-            userNameHtml = "<p class=\"user-name\">" + username + " 님</p>";
-        } else {
-            loginButtonHtml = "<li class=\"header__menu__item\"><a class=\"btn btn_contained btn_size_s\" href=\"/login\">로그인</a></li>";
-            registrationButtonText = "회원가입";
-            registrationButtonHref = "/registration";
-            userNameHtml = "";
+        loginButtonHtml = ""; // 로그인된 상태에서는 로그인 버튼을 숨김
+        registrationButtonText = "로그아웃";
+        registrationButtonHref = "/logout";
+        userNameHtml = "<p class=\"user-name\">" + username + " 님</p>";
+
+        // 글 제목 목록 받아오기
+        List<String> titles = PostDatabase.findAllTitleByUserId(userId);
+        StringBuilder titleList = new StringBuilder();
+        for (String title : titles) {
+            titleList.append("<li>").append(title).append("</li>\n");
         }
 
         // 문자열 대체
         template = template.replace("{username_placeholder}", userNameHtml)
                 .replace("{login_button_placeholder}", loginButtonHtml)
                 .replace("{registration_button_text}", registrationButtonText)
-                .replace("{registration_button_href}", registrationButtonHref);
+                .replace("{registration_button_href}", registrationButtonHref)
+                .replace("{title_placeholder}", titleList.toString());
+
+        return template;
+    }
+
+    private String generateDefaultIndexHtml() throws IOException {
+        String templateFilePath = "/index.html"; // 템플릿 파일 경로
+        ResourceUtil resourceUtil = new ResourceUtil();
+        String template = new String(resourceUtil.getByteArray(templateFilePath));
+
+        String loginButtonHtml, registrationButtonText, registrationButtonHref, userNameHtml;
+
+        loginButtonHtml = "<li class=\"header__menu__item\"><a class=\"btn btn_contained btn_size_s\" href=\"/login\">로그인</a></li>";
+        registrationButtonText = "회원가입";
+        registrationButtonHref = "/registration";
+        userNameHtml = "";
+
+        // 문자열 대체
+        template = template.replace("{username_placeholder}", userNameHtml)
+                .replace("{login_button_placeholder}", loginButtonHtml)
+                .replace("{registration_button_text}", registrationButtonText)
+                .replace("{registration_button_href}", registrationButtonHref)
+                .replace("{title_placeholder}", "");
 
         return template;
     }
