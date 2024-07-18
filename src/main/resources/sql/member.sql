59c0cea311d910b775857c632fb494579e221958
blaxsior <heejun2798@gmail.com>
2024-07-18 17:12:09 +0900

    feat: 메인 페이지 이미지 넘기기 기능 구현

---
+++ src/main/resources/sql/member.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Member (
+                        userId varchar(60) primary key,
+                        password varchar(100),
+                        name varchar(60),
+                        email varchar(60)
+);
