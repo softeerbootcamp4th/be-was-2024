51eb5a2056c16d6e903e0e70001f5352c2f76c8c
wjddn2165 <qowjddn2165@gmail.com>
2024-07-19 10:25:45 +0900

    메인화면에서 동적으로 게시글을 보여주도록 수정, 회원가입 시 중복 아이디 처리, 게시글 처리 핸들러 구현

--- src/main/java/webserver/Router.java
+++ src/main/java/webserver/Router.java
@@ -1,10 +1,17 @@
 package webserver;
 
-import db.Database;
 import enums.Status;
+import model.Article;
+import model.ArticleDao;
 import model.User;
+import model.UserDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import utils.*;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.sql.SQLException;
+import java.util.Base64;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +19,9 @@ import java.util.Map;
 public class Router {
     private final Map<String, Handler> getHandlersMap = new HashMap<>();
     private final Map<String, Handler> postHandlersMap = new HashMap<>();
+    private static final Logger logger = LoggerFactory.getLogger(Router.class);
+    private final UserDao userDao = new UserDao();
+    private final ArticleDao articleDAO = new ArticleDao();
     public Router() {
         initGetHandlers();
         initPostHandlers();
@@ -38,33 +48,97 @@ public class Router {
         getHandlersMap.put("/login", this::getLoginPage);
         getHandlersMap.put("/main", this::getMainPage);
         getHandlersMap.put("/user/list", this::getUserListPage);
+        getHandlersMap.put("/article", this::getArticlePage);
     }
 
     private void initPostHandlers() {
         postHandlersMap.put("/user/create", this::createUserHandler);
         postHandlersMap.put("/login", this::loginHandler);
         postHandlersMap.put("/logout", this::logoutHandler);
+        postHandlersMap.put("/article/create", this::createArticleHandler);
     }
 
-    private String createUserHandler(HttpRequestParser httpRequestParser, HttpResponseHandler httpResponseHandler, Model model) {
+    private String createUserHandler(HttpRequestParser httpRequestParser, HttpResponseHandler httpResponseHandler, Model model) throws IOException {
         httpResponseHandler.setStatus(Status.FOUND);
         Map<String, String> formData = getFormData(httpRequestParser);
 
-        String userId = formData.get("userId");
-        String password = formData.get("password");
-        String name = formData.get("name");
-        String email = formData.get("email");
+        String userId = URLDecoder.decode(formData.get("userId"), "UTF-8");
+        String password = URLDecoder.decode(formData.get("password"), "UTF-8");
+        String name = URLDecoder.decode(formData.get("name"), "UTF-8");
+        String email = URLDecoder.decode(formData.get("email"), "UTF-8");
+
+        try {
+            if (userDao.userExists(userId)) {
+                model.addAttribute("errorMessage", "이미 가입된 회원입니다.");
+                return "/registration/index";
+            }
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+        }
 
         User user = new User(userId, password, name, email);
-        Database.addUser(user);
+
+        try {
+            userDao.addUser(user);
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+        }
 
         return "redirect:/";
     }
+
+    private String createArticleHandler(HttpRequestParser httpRequestParser, HttpResponseHandler httpResponseHandler, Model model) {
+        httpResponseHandler.setStatus(Status.FOUND);
+
+        String sessionId = httpRequestParser.getCookiesMap().get("sid");
+        User user = SessionManager.getUser(sessionId);
+
+        String content = httpRequestParser.getParsedContent();
+        byte[] image = httpRequestParser.getParsedImage();
+
+        try {
+            articleDAO.addArticle(new Article(user.getUserId(), content, image));
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+        }
+
+        return "redirect:/";
+    }
+
 
     private String getMainPage(HttpRequestParser httpRequestParser, HttpResponseHandler httpResponseHandler, Model model) throws IOException {
         httpResponseHandler.setStatus(Status.OK);
 
         String sessionId = httpRequestParser.getCookiesMap().get("sid");
+
+        // 모든 게시글 가져와서 {{postList}}에 뿌려주기
+        StringBuilder postListHtml = new StringBuilder();
+        try {
+            Collection<Article> articles = articleDAO.getAllArticles();
+            for (Article article : articles) {
+                String decodedContent = URLDecoder.decode(article.getContent(), "UTF-8");
+                String base64Image = Base64.getEncoder().encodeToString(article.getImage());
+
+                postListHtml.append("<div class=\"post\">\n");
+                postListHtml.append("<div class=\"post__account\">\n");
+                postListHtml.append("<img class=\"post__account__img\"/>\n");
+                postListHtml.append("<p class=\"post__account__nickname\">").append(article.getAuthor()).append("</p>\n");
+                postListHtml.append("</div>\n");
+                postListHtml.append("<img class=\"post__img\" src=\"data:image/jpeg;base64,").append(base64Image).append("\" alt=\"Image\" />\n");
+                postListHtml.append("<div class=\"post__menu\">\n");
+                postListHtml.append("<ul class=\"post__menu__personal\">\n");
+                postListHtml.append("<li><button class=\"post__menu__btn\"><img src=\"./img/like.svg\" /></button></li>\n");
+                postListHtml.append("<li><button class=\"post__menu__btn\"><img src=\"./img/sendLink.svg\" /></button></li>\n");
+                postListHtml.append("</ul>\n");
+                postListHtml.append("<button class=\"post__menu__btn\"><img src=\"./img/bookMark.svg\" /></button>\n");
+                postListHtml.append("</div>\n");
+                postListHtml.append("<p class=\"post__article\">").append(decodedContent).append("</p>\n");
+                postListHtml.append("</div>\n");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        model.addAttribute("postList", postListHtml.toString());
 
         if (SessionManager.isValidSession(sessionId)) {
             // 로그인 한 유저
@@ -103,32 +177,46 @@ public class Router {
         return "/index";
     }
 
+
     private String getUserListPage(HttpRequestParser httpRequestParser, HttpResponseHandler httpResponseHandler, Model model) throws IOException {
         String sessionId = httpRequestParser.getCookiesMap().get("sid");
 
         if (SessionManager.isValidSession(sessionId)) {
             httpResponseHandler.setStatus(Status.OK);
-            Collection<User> userList = Database.findAll();
-            StringBuilder userListBuilder = new StringBuilder();
-            for (User user : userList) {
-                userListBuilder.append("<tr>")
-                        .append("<td>").append(user.getUserId()).append("</td>")
-                        .append("<td>").append(user.getName()).append("</td>")
-                        .append("<td>").append(user.getEmail()).append("</td>")
-                        .append("</tr>");
-            }
+            try {
+                Collection<User> userList = userDao.getAllUsers();
+                StringBuilder userListBuilder = new StringBuilder();
+                for (User user : userList) {
+                    userListBuilder.append("<tr>")
+                            .append("<td>").append(user.getUserId()).append("</td>")
+                            .append("<td>").append(user.getName()).append("</td>")
+                            .append("<td>").append(user.getEmail()).append("</td>")
+                            .append("</tr>");
+                }
 
-            model.addAttribute("userList", userListBuilder.toString());
+                model.addAttribute("userList", userListBuilder.toString());
+            } catch (SQLException e) {
+                logger.error(e.getMessage());
+            }
 
             // 유저 정보를 HTML 테이블 행으로 변환
             return "/user_list";
-
         } else {
-            httpResponseHandler.setStatus(Status.NOT_FOUND);
-            return "/invalid";
+            httpResponseHandler.setStatus(Status.FOUND);
+            return "redirect:/login";
         }
+    }
 
+    private String getArticlePage(HttpRequestParser httpRequestParser, HttpResponseHandler httpResponseHandler, Model model) throws IOException {
+        String sessionId = httpRequestParser.getCookiesMap().get("sid");
 
+        if (SessionManager.isValidSession(sessionId)) {
+            httpResponseHandler.setStatus(Status.OK);
+            return "/article/index";
+        } else {
+            httpResponseHandler.setStatus(Status.FOUND);
+            return "redirect:/login";
+        }
     }
 
     private String getLoginPage(HttpRequestParser httpRequestParser, HttpResponseHandler httpResponseHandler, Model model) throws IOException {
@@ -146,10 +234,16 @@ public class Router {
     private String loginHandler(HttpRequestParser httpRequestParser, HttpResponseHandler httpResponseHandler, Model model) throws IOException {
         Map<String, String> formData = getFormData(httpRequestParser);
 
-        String userId = formData.get("userId");
-        String password = formData.get("password");
+        String userId = URLDecoder.decode(formData.get("userId"), "UTF-8");
+        String password = URLDecoder.decode(formData.get("password"), "UTF-8");
 
-        User user = Database.findUserById(userId);
+        User user = null;
+
+        try {
+            user = userDao.getUser(userId);
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+        }
 
         // 로그인 성공
         if (user != null && user.getPassword().equals(password)) {
@@ -177,6 +271,7 @@ public class Router {
         Map<String, String> cookiesMap = httpRequestParser.getCookiesMap();
         String sessionId = cookiesMap.get("sid");
         if (sessionId != null) {
+            SessionManager.removeSession(sessionId);
             Cookie cookie = new Cookie.Builder("sid", sessionId)
                     .path("/")
                     .maxAge(0)
