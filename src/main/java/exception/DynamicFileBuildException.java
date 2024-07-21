fbc721b8b66a72a09aab13a22ba18061ffb91dfc
hyeokson <chanhuk1765@gmail.com>
2024-07-21 19:26:50 +0900

    docs: DynamicFileBuildException 클래스 주석 추가

--- src/main/java/exception/DynamicFileBuildException.java
+++ src/main/java/exception/DynamicFileBuildException.java
@@ -1,6 +1,8 @@
 package exception;
 
-// 동적 파일 생성 중에 발생하는 예외
+/**
+ * 동적 파일 생성 중에 발생하는 예외 클래스
+ */
 public class DynamicFileBuildException extends RuntimeException {
     public DynamicFileBuildException(String message) {
         super(message);
