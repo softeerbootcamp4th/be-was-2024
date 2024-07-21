07a67e066e68683d5765bac2ce428b542694bb33
hyeokson <chanhuk1765@gmail.com>
2024-07-21 19:10:40 +0900

    docs: ResourceNotFoundException 주석 추가

--- src/main/java/exception/ResourceNotFoundException.java
+++ src/main/java/exception/ResourceNotFoundException.java
@@ -1,5 +1,8 @@
 package exception;
 
+/**
+ * 특정 파일을 찾을 수 없을 경우 발생하는 예외 클래스
+ */
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
