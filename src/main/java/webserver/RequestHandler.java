79d6eddf269d7bdc5eefab5b0e1ccfc53f3634e8
hyeokson <chanhuk1765@gmail.com>
2024-07-21 19:37:04 +0900

    docs: RequestHandler 클래스 주석 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,6 +5,9 @@ import java.net.Socket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * InputStream, OutputStream을 생성하고 Dispatcher에게 작업을 위임하는 클래스
+ */
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
@@ -14,6 +17,11 @@ public class RequestHandler implements Runnable {
         this.connection = connectionSocket;
     }
 
+    /**
+     *  InputStream, OutputStream을 생성한다.
+     *  생성된 두 stream을 BufferedInputStream, DataOutputStream으로 감싸고
+     *  Dispatcher에게 인수로 넘겨주어 HttpRequest를 처리한다.
+     */
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
