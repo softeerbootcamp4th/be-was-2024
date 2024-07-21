fd3bed863afbc63d3bbef7277bfcf5cc8f26d1e9
hyeokson <chanhuk1765@gmail.com>
2024-07-21 19:07:42 +0900

    docs: User 클래스 주석 추가

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -4,6 +4,9 @@ import exception.InvalidHttpRequestException;
 
 import java.util.Map;
 
+/**
+ * 유저의 정보를 저장하는 클래스
+ */
 public class User implements MyTagDomain{
     private int id;
     private final String userId;
@@ -35,7 +38,15 @@ public class User implements MyTagDomain{
         );
     }
 
-    private static String getValueFromMap(Map<String, String> map, String key) {
+    /**
+     * 유저의 특정 필드 값을 map에서 꺼내서 반환한다.
+     * 필드 값이 null이거나 비어있다면 예외가 발생한다.
+     *
+     * @param map : 유저의 정보가 저장된 map
+     * @param key : 유저 클래스의 필드 이름
+     * @return : 유저 클래스의 필드 값
+     */
+    public static String getValueFromMap(Map<String, String> map, String key) {
         String value = map.get(key);
         if (value == null || value.isEmpty()) {
             throw new InvalidHttpRequestException("User information cannot be null");
