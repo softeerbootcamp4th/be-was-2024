125f824a472bbaf20a3b766578a9c8632edca5f4
hyeokson <chanhuk1765@gmail.com>
2024-07-21 19:23:48 +0900

    docs: NULL, text/plain 값 추가 / of 메서드 추가

--- src/main/java/constant/FileExtensionType.java
+++ src/main/java/constant/FileExtensionType.java
@@ -4,7 +4,9 @@ package constant;
  * File 확장자 타입 Enum
  */
 public enum FileExtensionType {
+    NULL(""),
     HTML("text/html"),
+    PLAIN("text/plain"),
     CSS("text/css"),
     JS("application/javascript"),
     ICO("image/x-icon"),
@@ -20,5 +22,14 @@ public enum FileExtensionType {
 
     public String getContentType() {
         return contentType;
+    }
+
+    public static FileExtensionType of(String contentType) {
+        for (FileExtensionType fileType : values()) {
+            if (fileType.getContentType().equals(contentType)) {
+                return fileType;
+            }
+        }
+        return null;
     }
 }
